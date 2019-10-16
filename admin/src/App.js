4d3d337425201d39041be02e7052d1031e2efd6a
Suparada Saitalae <ann.suparada@gmail.com>
2019-10-16 11:03:37 -0500

    ready to deploy

---
+++ admin/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Speak Out is maintaining the website</h1>
+       <p>Coming Soon...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
