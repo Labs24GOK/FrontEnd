40ca1692cc321cac87ab5aa38f78195f9fc9baed
Suparada Saitalae <ann.suparada@gmail.com>
2019-11-05 10:02:44 -0600

    styled component

--- client/src/styles/styledComponents.js
+++ client/src/styles/styledComponents.js
@@ -10,17 +10,51 @@ export const BackButton = styled.div`
     cursor:"pointer"
 `;
 export const HeaderWrapper = styled.div`
-
+    text-align: center
 `;
 
 export const BigTitle = styled.h2`
-    width: 70%;
-    margin: 30px 0 0 0;
+    margin: 0 0 5px 0;
 `;
 
 export const SmallTitle = styled.p`
-    width: 70%;
-    margin: 0 0 0 0;
+    margin: 0 0 2px 0;
 `;
+
+export const Row = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    text-align: left;
+`;
+export const RowRight = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+`;
+
+export const Column4 = styled.div`
+    width: 25%;
+    margin: 7px;
+`;
+
+export const Column2 = styled.div`
+    width: 50%;
+    margin: 7px;
+`;
+
+export const Column3 = styled.div`
+    width: 33%;
+    margin: 7px;
+`;
+
+export const H4 = styled.h4`
+margin: 0 0 2px 0;
+`;
+
+export const P = styled.p`
+
+`;
+
 
 
