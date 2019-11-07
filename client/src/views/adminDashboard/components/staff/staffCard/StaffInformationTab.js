24a36da1c049335f70290679ca941030c976d4fd
Bilguun Tse <bbg.1015@gmail.com>
2019-11-07 12:55:33 -0600

    displaying coursesTab and has sorting functionality

--- client/src/views/adminDashboard/components/staff/staffCard/StaffInformationTab.js
+++ client/src/views/adminDashboard/components/staff/staffCard/StaffInformationTab.js
@@ -1,13 +1,12 @@
-import React, {useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Grid, Segment, Form, Icon, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 import { getStaffById, toggleStaffEditComponent } from '../../../../../actions';
-import{ withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import StaffForm from './StaffForm';
 
 
 const StaffInformationTab = props => {
-    // console.log('staff info', )
     useEffect(() => {
         props.getStaffById(props.staffID)
     }, [])
@@ -15,109 +14,105 @@ const StaffInformationTab = props => {
     const [edit, setEdit] = useState(false)
 
     let options = { year: 'numeric', month: 'numeric', day: 'numeric' }; //'long'
-    let birthdate = new Date(props.staffById.birthdate).toLocaleDateString('en-US', options) 
-    
-    
+    let birthdate = new Date(props.staffById.birthdate).toLocaleDateString('en-US', options)
+
+
     const editStaffInfo = e => {
         console.log(editStaffInfo)
         e.preventDefault();
         props.toggleStaffEditComponent();
-   }
+    }
 
-    return(
+    return (
         <div className="gridView">
             {
                 !props.isEditing ?
-            // <form className='ui form' >
-            <Grid columns='equal'>
+                    <Grid columns='equal'>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Segment>Staff ID</Segment>
+                                <Segment>{props.staffById.id}</Segment>
+                            </Grid.Column>
 
-            <Grid.Row>
-                 <Grid.Column>
-                    <Segment>Staff ID</Segment>
-                    <Segment>{props.staffById.id}</Segment>
-                </Grid.Column>
+                            <Grid.Column>
+                                <Segment>Name</Segment>
+                                <Segment>{props.staffById.name}</Segment>
+                            </Grid.Column>
 
-                <Grid.Column>
-                    <Segment>Name</Segment>
-                    <Segment>{props.staffById.name}</Segment>
-                </Grid.Column>
+                            <Grid.Column>
+                                <Segment>Short Name</Segment>
+                                <Segment>{props.staffById.short_name}</Segment>
+                            </Grid.Column>
 
-                <Grid.Column>
-                    <Segment>Short Name</Segment>
-                    <Segment>{props.staffById.short_name}</Segment>
-                </Grid.Column>
+                            <Grid.Column>
+                                <Segment>CPR</Segment>
+                                <Segment>{props.staffById.cpr}</Segment>
+                            </Grid.Column>
 
-                <Grid.Column>
-                    <Segment>CPR</Segment>
-                    <Segment>{props.staffById.cpr}</Segment>
-                </Grid.Column>
-
-                <Grid.Column>
-                    <Segment style={{color: "#26ABBD", cursor:"pointer", width:"fit-content"}} onClick={editStaffInfo}> 
-                    <Icon name="edit" style = {{color:"#26ABBD",cursor:"pointer"}}/> Edit 
+                            <Grid.Column>
+                                <Segment style={{ color: "#26ABBD", cursor: "pointer", width: "fit-content" }} onClick={editStaffInfo}>
+                                    <Icon name="edit" style={{ color: "#26ABBD", cursor: "pointer" }} /> Edit
                     </Segment>
-                </Grid.Column>
-            </Grid.Row>
+                            </Grid.Column>
+                        </Grid.Row>
 
 
-            <Grid.Row>
-                <Grid.Column>
-                    <Segment>Mobile Number</Segment>
-                    <Segment>{props.staffById.mobile_number}</Segment>
-                </Grid.Column>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Segment>Mobile Number</Segment>
+                                <Segment>{props.staffById.mobile_number}</Segment>
+                            </Grid.Column>
 
-                <Grid.Column>
-                    <Segment>Accent</Segment>
-                    <Segment>{props.staffById.accent}</Segment>
-                </Grid.Column>
+                            <Grid.Column>
+                                <Segment>Accent</Segment>
+                                <Segment>{props.staffById.accent}</Segment>
+                            </Grid.Column>
 
-                 <Grid.Column>
-                    <Segment>Gender</Segment>
-                    <Segment>{props.staffById.gender}</Segment>
-                </Grid.Column>
+                            <Grid.Column>
+                                <Segment>Gender</Segment>
+                                <Segment>{props.staffById.gender}</Segment>
+                            </Grid.Column>
 
-                <Grid.Column>
-                    <Segment>Birth date</Segment>
-                    <Segment>{birthdate}</Segment>
-                </Grid.Column>
+                            <Grid.Column>
+                                <Segment>Birth date</Segment>
+                                <Segment>{birthdate}</Segment>
+                            </Grid.Column>
 
-                <Grid.Column>
-                </Grid.Column>
-            </Grid.Row>
+                            <Grid.Column>
+                            </Grid.Column>
+                        </Grid.Row>
 
-            <Grid.Row>
-                <Grid.Column>
-                    <Segment>Teaching Rate</Segment>
-                    <Segment>{props.staffById.teaching_rate}</Segment>
-                </Grid.Column>
-                <Grid.Column>
-                    <Segment>Admin</Segment>
-                    <Segment>{props.staffById.admin}</Segment>
-                </Grid.Column>
-                <Grid.Column>
-                    <Segment>Active</Segment>
-                    <Segment>{props.staffById.active}</Segment>
-                </Grid.Column>
-                <Grid.Column>
-                    <Segment>User ID</Segment>
-                    <Segment>{props.staffById.user_id}</Segment>
-                </Grid.Column>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Segment>Teaching Rate</Segment>
+                                <Segment>{props.staffById.teaching_rate}</Segment>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Segment>Admin</Segment>
+                                <Segment>{props.staffById.admin}</Segment>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Segment>Active</Segment>
+                                <Segment>{props.staffById.active}</Segment>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Segment>User ID</Segment>
+                                <Segment>{props.staffById.user_id}</Segment>
+                            </Grid.Column>
 
-                <Grid.Column>
+                            <Grid.Column>
 
-                </Grid.Column> 
+                            </Grid.Column>
 
-                </Grid.Row>
+                        </Grid.Row>
 
-            </Grid>
-            // </form>
-         : <StaffForm {...props} />
-        }
-        
-    </div>
+                    </Grid>
+                    : <StaffForm {...props} />
+            }
+
+        </div>
     )
 }
-
 
 const mapStateToProps = state => {
     return {
@@ -125,13 +120,13 @@ const mapStateToProps = state => {
         staffById: state.staffByIdReducer.staffById,
         isEditing: state.staffByIdReducer.isEditing,
     };
-  };
+};
 
 
-  
-  export default withRouter(
+
+export default withRouter(
     connect(
         mapStateToProps,
-      { getStaffById, toggleStaffEditComponent}
-  )(StaffInformationTab)
-  )
+        { getStaffById, toggleStaffEditComponent }
+    )(StaffInformationTab)
+)
--- client/src/views/adminDashboard/components/students/studentCard/StudentCoursesTab.js
+++ client/src/views/adminDashboard/components/students/studentCard/StudentCoursesTab.js
@@ -2,43 +2,65 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
 import { getStudentTable } from '../../../../../actions';
-import { Table } from 'semantic-ui-react';
+import { Table } from 'antd';
+
+import './studentTable.scss'
 
 const StudentCoursesTab = props => {
+  const columns = [
+    {
+      title: 'Term',
+      dataIndex: 'id',
+      key: 1,
+      sorter: (a, b) => a.id - b.id,
+      sortDirections: ['ascend']
+    },
+    {
+      title: 'Days',
+      dataIndex: 'cpr',
+      key: 2,
+      sorter: (a, b) => b.cpr - a.cpr,
+      sortDirections: ['descend']
+    },
+    {
+    title: 'Type',
+    dataIndex: 'first_name',
+    key: 3,
+    sorter: (a, b) => {
+      return (b.first_name===null)-(a.first_name===null) || +(a.first_name>b.first_name)||-(a.first_name<b.first_name);
+    },
+    sortDirections: ['descend']
+    },
+    {
+      title: 'Group',
+      dataIndex: 'additional_names',
+      key: 4,
+    },
+    {
+      title: 'Level',
+      dataIndex: 'gender',
+      key: 5,
+    },
+    {
+      title: 'Section',
+      dataIndex: 'mobile_telephone',
+      key: 6,
+    },
+    {
+      title: 'Subsection',
+      dataIndex: 'mobile_telephone',
+      key: 7,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'mobile_telephone',
+      key: 8,
+    },
+  ];
+
   return (
     <>
-      <Table selectable singleLine>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Term</Table.HeaderCell>
-            <Table.HeaderCell>Days</Table.HeaderCell>
-            <Table.HeaderCell>Type</Table.HeaderCell>
-            <Table.HeaderCell>Group</Table.HeaderCell>
-            <Table.HeaderCell>Level</Table.HeaderCell>
-            <Table.HeaderCell>Section</Table.HeaderCell>
-            <Table.HeaderCell>Subsection</Table.HeaderCell>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {
-            props.studentList.map(each => {
-              return (
-                <Table.Row>
-                  <Table.Cell>{each.id}</Table.Cell>
-                  <Table.Cell>{each.cpr}</Table.Cell>
-                  <Table.Cell>{each.first_name}</Table.Cell>
-                  <Table.Cell>{each.additional_names}</Table.Cell>
-                  <Table.Cell>{each.gender}</Table.Cell>
-                  <Table.Cell>{each.mobile_telephone}</Table.Cell>
-                  <Table.Cell>{each.id}</Table.Cell>
-                  <Table.Cell>Completed</Table.Cell>
-                </Table.Row>
-                )
-            })
-          }
-        </Table.Body>
-      </Table>
+      <Table dataSource={props.studentList} className="coursesTable" columns={columns} pagination={false} />
     </>
   )
 }
