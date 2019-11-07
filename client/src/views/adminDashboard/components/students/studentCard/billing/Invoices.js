59c1ff2e4b768880f38c5e46e581e41cefb6d24a
Suparada Saitalae <ann.suparada@gmail.com>
2019-11-07 15:05:51 -0600

    billing styled done

--- client/src/views/adminDashboard/components/students/studentCard/billing/Invoices.js
+++ client/src/views/adminDashboard/components/students/studentCard/billing/Invoices.js
@@ -1,42 +1,91 @@
 import React from 'react';
-import { Table, Badge, Menu, Dropdown, Icon } from 'antd';
+import { Row, Col } from 'antd';
+import './billing.scss'
 
 const Invoices = props => {
-    const columns = [
-        {
-          title: 'Course ID',
-          dataIndex: 'course_id',
-          key: 1,
-        },
-        {
-          title: 'Item ID',
-          dataIndex: 'item_id',
-          key: 2,
-        },
-        {
-        title: 'Quantity',
-        dataIndex: 'quantity',
-        key: 3,
-        },
-        {
-          title: 'Invoice number',
-          dataIndex: 'invoice_number',
-          key: 4,
-        }
-      ];
     
       const data = [
-        {
+        {invoice: [{
             course_id: 231,
             item_id: 441,
             quantity: "x2",
-            invoice_number: 993
-        }
+            invoice_number: 993,
+            payment_due: "11/11/19",
+            amount: 200,
+            notes: "Thank you"
+            }]
+        },
+        {invoice: [{
+            course_id: 231,
+            item_id: 441,
+            quantity: "x2",
+            invoice_number: 993,
+            payment_due: "11/11/19",
+            amount: 200,
+            notes: "Thank you"
+            }]
+        },
+        {invoice: [{
+            course_id: 231,
+            item_id: 441,
+            quantity: "x2",
+            invoice_number: 993,
+            payment_due: "11/11/19",
+            amount: 200,
+            notes: "Thank you"
+            }]
+        },
+        {invoice: [{
+            course_id: 231,
+            item_id: 441,
+            quantity: "x2",
+            invoice_number: 993,
+            payment_due: "11/11/19",
+            amount: 200,
+            notes: "Thank you"
+            }]
+        },
+        
       ]
+    //   const 
 
     return (
         <>
-        <Table columns={columns} dataSource={data} pagination={false} />
+            {data.map(item => (
+                <div>
+            <Row  className="invoice-top-row" type="flex" justify="start">
+                    <Col span={3}><h4>Course ID</h4></Col>
+                    <Col span={3}><h4>Item ID</h4></Col>
+                    <Col span={3}><h4>Quantity</h4></Col>
+                    <Col span={3}><h4>Invoice number</h4></Col>
+                </Row>
+                <Row type="flex" justify="start">
+                    <Col span={3}>{item.invoice[0].course_id}</Col>
+                    <Col span={3}>{item.invoice[0].item_id}</Col>
+                    <Col span={3}>{item.invoice[0].quantity}</Col>
+                    <Col span={3}>{item.invoice[0].invoice_number}</Col>
+                </Row>
+                <Row type="flex" justify="end">
+                    <Col span={3}><h4>Payment Due</h4></Col>
+                </Row>
+                <Row type="flex" justify="end">
+                    <Col span={3}>{item.invoice[0].payment_due}</Col>
+                </Row>
+                <Row className="invoice-amount-row"type="flex" justify="space-between">
+                    <Col span={3}><h4>Note</h4></Col>
+                    <Col span={3}></Col>
+                    <Col span={3}></Col>
+                    <Col span={3}><h4>Amount</h4></Col>
+                </Row>
+                <Row className="invoice-bottom-row" type="flex" justify="space-between">
+                    <Col span={3}>{item.invoice[0].notes}</Col>
+                    <Col span={3}></Col>
+                    <Col span={3}></Col>
+                    <Col span={3}>{item.invoice[0].amount}</Col>
+                </Row>
+            </div>
+            ))}
+           
         </>
     )
 }
--- client/src/views/adminDashboard/components/students/studentCard/billing/Receipts.js
+++ client/src/views/adminDashboard/components/students/studentCard/billing/Receipts.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import { Tab } from 'semantic-ui-react';
+import { Row, Col } from 'antd';
 
 const Receipts = props => {
     
+  
     
     return (
         <>
-            Receipts
+            <div>
+                <Row type="flex" justify="start" style={{ marginBottom: '15px', background: '#dff2f4', padding: '3px' }}>
+                    <Col span={3}><h3>Course ID #12</h3></Col>
+                </Row>
+                <Row type="flex" justify="space-between" style={{ marginBottom: '5px' }}>
+                    <Col span={3}><h4>Items</h4></Col>
+                    <Col span={3}><h4>Quanitity</h4></Col>
+                    <Col span={3}><h4>Price</h4></Col>
+                </Row>
+                <Row type="flex" justify="space-between" style={{ marginBottom: '5px' }}>
+                    <Col span={3}><p>Registration</p></Col>
+                    <Col span={3}><p>x1</p></Col>
+                    <Col span={3}><p>20.00</p></Col>
+                </Row>
+                <Row type="flex" justify="space-between" style={{ marginBottom: '5px' }}>
+                    <Col span={3}><p>Book</p></Col>
+                    <Col span={3}><p>x0.65</p></Col>
+                    <Col span={3}><p>4.00</p></Col>
+                </Row>
+                <Row type="flex" justify="space-between" style={{ marginBottom: '5px' }}>
+                    <Col span={3}><p>Stationary Kit</p></Col>
+                    <Col span={3}><p>0.80</p></Col>
+                    <Col span={3}><p>8.00</p></Col>
+                </Row>
+                <Row type="flex" justify="space-between" style={{ marginBottom: '5px' }}>
+                    <Col span={3}><p>Course</p></Col>
+                    <Col span={3}><p>x0.80</p></Col>
+                    <Col span={3}><p>50.00</p></Col>
+                </Row>
+            </div>
+            <Row type="flex" justify="start" style={{ marginTop: '15px' }}>
+                <Col span={3}><h4>Note</h4></Col>
+            </Row>
+            <Row type="flex" justify="start">
+                <Col ><p>Amir has requested a hard cover book</p></Col>
+            </Row>
+            <Row type="flex" justify="end">
+                <Col span={3}>TOTAL<h3>$164.00</h3></Col>
+            </Row>
         </>
     )
 }
