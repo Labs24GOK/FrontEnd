2f45eaa146feda1ed8077fe287e06d18f0831391
Suparada Saitalae <ann.suparada@gmail.com>
2019-11-15 19:18:39 -0600

    fixed navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Routes from './routes/Routes';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
-import './App.css';
 
 library.add(faAngleLeft)
 
