e20532905ca8a214000bc72d8ffd0af5d7fb5c50
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-11-16 20:28:21 -0500

    all sorters gone

--- client/src/views/adminDashboard/components/staff/staffCard/StaffCoursesTab.js
+++ client/src/views/adminDashboard/components/staff/staffCard/StaffCoursesTab.js
@@ -25,24 +25,16 @@ const StaffCoursesTab = props => {
             title: 'Term',
             dataIndex: 'term',
             key: 3,
-            sorter: (a, b) => a.id - b.id,
-            sortDirections: ['ascend']
         },
         {
             title: 'Days',
             dataIndex: 'term',
             key: 4,
-            sorter: (a, b) => b.cpr - a.cpr,
-            sortDirections: ['descend'],
         },
         {
             title: 'Type',
             dataIndex: 'course_type',
             key: 5,
-            sorter: (a, b) => {
-                return (b.first_name === null) - (a.first_name === null) || +(a.first_name > b.first_name) || -(a.first_name < b.first_name);
-            },
-            sortDirections: ['descend']
         },
         {
             title: 'Group Type',
