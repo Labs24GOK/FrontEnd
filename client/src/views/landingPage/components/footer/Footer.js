f92cf6611fc74162427cb44f767e32ec7d235e6d
Suparada Saitalae <ann.suparada@gmail.com>
2019-11-17 10:26:45 -0600

    add link to footer

--- client/src/views/landingPage/components/footer/Footer.js
+++ client/src/views/landingPage/components/footer/Footer.js
@@ -13,13 +13,13 @@ const Footer = () => {
     <footer>
       <div className="contacts">
         <div className="icons">
-          <a href="#">
+          <a href="https://www.instagram.com/speak_out_program/">
             <img src={Instagram} />
           </a>
-          <a href="#">
+          <a href="https://www.facebook.com/speakoutinfobh/?ref=bookmarks" target="_blank">
             <img src={Facebook} />
           </a>
-          <a href="#">
+          <a href="https://twitter.com/speakoutbh" target="_blank">
             <img src={Twitter} />
           </a>
           <a href="#">
@@ -27,8 +27,12 @@ const Footer = () => {
           </a>
         </div>
         <div className="address">
+          <a href="https://www.google.com/maps/place/Rd+No+3949,+Bani+Jamra,+Bahrain/@26.2109242,50.4587409,17z/data=!3m1!4b1!4m5!3m4!1s0x3e49ba6f42294655:0xff144caac5b9e0cc!8m2!3d26.2109194!4d50.4609296"
+           target="_blank">
           <img src={Pin} />
+          </a>
           <p>Rd No 3949, Bani Jamra, Bahrain</p>
+          
         </div>
         <div className="email">
           <img src={Email} />
