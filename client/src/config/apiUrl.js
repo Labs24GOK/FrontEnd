f2391f990f5053c6d50e8539caeac0ba7bbfdc46
Eric Richards <esholundr@yahoo.com>
2020-02-06 13:36:56 -0700

    added config directory with apiUrl file

---
+++ client/src/config/apiUrl.js
@@ -0,0 +1,5 @@
+const productionURL = `https://speakout-lambda.herokuapp.com`;
+const stagingURL = `https://speakout-lambda-staging.herokuapp.com`;
+const localhost = `http://localhost:4000`;
+
+export const API_URL = localhost;
