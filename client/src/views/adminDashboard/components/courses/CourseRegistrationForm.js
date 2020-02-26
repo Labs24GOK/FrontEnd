8738c18b92791e21b38c2a328a6b75fc73853ea6
Jessica Cooper <jessicagcooper@gmail.com>
2020-02-26 17:35:37 -0600

    Added red border validation to add course form.  Not on notes or grade(in grade it prevented N/A from automatically being submitted).  Notes still needs to be allowed to be empty on the backend.

--- client/src/views/adminDashboard/components/courses/CourseRegistrationForm.js
+++ client/src/views/adminDashboard/components/courses/CourseRegistrationForm.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import { getDropDownCourses, addCourse } from '../../../../actions';
-import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
+import React, { useEffect, useState } from 'react';
+import Dropdown from 'react-dropdown';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { addCourse, getDropDownCourses } from '../../../../actions';
 import {
+  Button,
+  ButtonDiv,
+  Div,
+  FormSet,
   FormWrap,
   Input,
-  Button,
   Label,
-  FormSet,
-  Div,
-  ButtonDiv,
 } from '../mainStyle/styledComponent.js';
 
 const CourseRegistrationForm = props => {
@@ -20,7 +20,7 @@ const CourseRegistrationForm = props => {
     props.getDropDownCourses();
   }, []);
 
-  console.log("SCHOOL GRADE TABLE", props.schoolGradeDropdown)
+  console.log('SCHOOL GRADE TABLE', props.schoolGradeDropdown);
 
   const [course, setCourse] = useState({
     term_id: '',
@@ -41,28 +41,11 @@ const CourseRegistrationForm = props => {
     notes: '',
   });
 
-  const [grade, setGrade] = useState(0)
-  console.log("GRADE", grade)
+  const [grade, setGrade] = useState(0);
+  console.log('GRADE', grade);
 
   const status = ['Active', 'Completed', 'Waitlist', 'Cancelled'];
   const section = ['A', 'B', 'C'];
-
-  // const [touched, setTouched] = useState({
-  //   term_id: false,
-  //   course_type_id: false,
-  //   group_type_id: false,
-  //   school_grade_id: false,
-  //   level_id: false,
-  //   course_schedule_id: false,
-  //   room_id: false,
-  //   teacher_id: false,
-  //   section: false,
-  //   hourly_rate: false,
-  //   start_time: false,
-  //   end_time: false,
-  //   notes: false,
-  //   status: false,
-  // });
 
   function handleChange(event) {
     setCourse({
@@ -71,11 +54,124 @@ const CourseRegistrationForm = props => {
     });
   }
 
+  //validation
+  const [errorBorderTerm, setErrorBorderTerm] = useState('transparent'); //error #C73642
+  const [errorBorderCourse, setErrorBorderCourse] = useState('transparent'); //error #C73642
+  const [errorBorderGroup, setErrorBorderGroup] = useState('transparent'); //error #C73642
+  const [errorBorderLevel, setErrorBorderLevel] = useState('transparent'); //error #C73642
+  const [errorBorderSection, setErrorBorderSection] = useState('transparent'); //error #C73642
+  // const [errorBorderGrade, setErrorBorderGrade] = useState('transparent'); //error #C73642
+  const [errorBorderSchedule, setErrorBorderSchedule] = useState('transparent'); //error #C73642
+  const [errorBorderStartDate, setErrorBorderStartDate] = useState(
+    'transparent'
+  ); //error #C73642
+  const [errorBorderEndDate, setErrorBorderEndDate] = useState('transparent'); //error #C73642
+  const [errorBorderStartTime, setErrorBorderStartTime] = useState(
+    'transparent'
+  ); //error #C73642
+  const [errorBorderEndTime, setErrorBorderEndTime] = useState('transparent'); //error #C73642
+  const [errorBorderRoom, setErrorBorderRoom] = useState('transparent'); //error #C73642
+  const [errorBorderTeacher, setErrorBorderTeacher] = useState('transparent'); //error #C73642
+  const [errorBorderHourlyRate, setErrorBorderHourlyRate] = useState(
+    'transparent'
+  ); //error #C73642
+  const [errorBorderStatus, setErrorBorderStatus] = useState('transparent'); //error #C73642
+
   function handleSubmit(event) {
     console.log('CREATE COURSE BODY', course);
     event.preventDefault();
-    props.addCourse(course);
-    props.setForm(false);
+
+    //check for required fields
+    if (
+      course.term_id === '' ||
+      course.course_type_id === '' ||
+      course.group_type_id === '' ||
+      course.level_id === '' ||
+      course.section === '' ||
+      course.course_schedule_id === '' ||
+      course.start_date === '' ||
+      course.end_date === '' ||
+      course.start_time === '' ||
+      course.end_time === '' ||
+      course.room_id === '' ||
+      course.teacher_id === '' ||
+      course.hourly_rate === '' ||
+      course.status === ''
+    ) {
+      if (course.term_id === '') {
+        setErrorBorderTerm('#ef6570');
+      } else {
+        setErrorBorderTerm('transparent');
+      }
+      if (course.course_type_id === '') {
+        setErrorBorderCourse('#ef6570');
+      } else {
+        setErrorBorderCourse('transparent');
+      }
+      if (course.group_type_id === '') {
+        setErrorBorderGroup('#ef6570');
+      } else {
+        setErrorBorderGroup('transparent');
+      }
+      if (course.level_id === '') {
+        setErrorBorderLevel('#ef6570');
+      } else {
+        setErrorBorderLevel('transparent');
+      }
+      if (course.section === '') {
+        setErrorBorderSection('#ef6570');
+      } else {
+        setErrorBorderSection('transparent');
+      }
+      if (course.course_schedule_id === '') {
+        setErrorBorderSchedule('#ef6570');
+      } else {
+        setErrorBorderSchedule('transparent');
+      }
+      if (course.start_date === '') {
+        setErrorBorderStartDate('#ef6570');
+      } else {
+        setErrorBorderStartDate('transparent');
+      }
+      if (course.end_date === '') {
+        setErrorBorderEndDate('#ef6570');
+      } else {
+        setErrorBorderEndDate('transparent');
+      }
+      if (course.start_time === '') {
+        setErrorBorderStartTime('#ef6570');
+      } else {
+        setErrorBorderStartTime('transparent');
+      }
+      if (course.end_time === '') {
+        setErrorBorderEndTime('#ef6570');
+      } else {
+        setErrorBorderEndTime('transparent');
+      }
+      if (course.room_id === '') {
+        setErrorBorderRoom('#ef6570');
+      } else {
+        setErrorBorderRoom('transparent');
+      }
+      if (course.teacher_id === '') {
+        setErrorBorderTeacher('#ef6570');
+      } else {
+        setErrorBorderTeacher('transparent');
+      }
+      if (course.hourly_rate === '') {
+        setErrorBorderHourlyRate('#ef6570');
+      } else {
+        setErrorBorderHourlyRate('transparent');
+      }
+      if (course.status === '') {
+        setErrorBorderStatus('#ef6570');
+      } else {
+        setErrorBorderStatus('transparent');
+      }
+    } else {
+      props.addCourse(course);
+      props.setForm(false);
+    }
   }
 
   const handleCancel = event => {
@@ -89,33 +185,54 @@ const CourseRegistrationForm = props => {
         <Div>
           <div>
             <Label>Term</Label>
-            <Dropdown
-              value={course.term_id}
-              onChange={e => setCourse({ ...course, term_id: e })}
-              controlClassName='myControlClassName'
-              className='dropdown'
-              options={props.termDropdown}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderTerm}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Dropdown
+                value={course.term_id}
+                onChange={e => setCourse({ ...course, term_id: e })}
+                controlClassName='myControlClassName'
+                className='dropdown'
+                options={props.termDropdown}
+              />
+            </div>
           </div>
           <div>
             <Label>Course Type</Label>
-            <Dropdown
-              value={course.course_type_id}
-              onChange={e => setCourse({ ...course, course_type_id: e })}
-              controlClassName='myControlClassName'
-              className='dropdown'
-              options={props.courseTypeDropdown}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderCourse}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Dropdown
+                value={course.course_type_id}
+                onChange={e => setCourse({ ...course, course_type_id: e })}
+                controlClassName='myControlClassName'
+                className='dropdown'
+                options={props.courseTypeDropdown}
+              />
+            </div>
           </div>
           <div>
             <Label>Group Type</Label>
-            <Dropdown
-              value={course.group_type_id}
-              onChange={e => setCourse({ ...course, group_type_id: e })}
-              controlClassName='myControlClassName'
-              className='dropdown'
-              options={props.groupTypeDropdown}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderGroup}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Dropdown
+                value={course.group_type_id}
+                onChange={e => setCourse({ ...course, group_type_id: e })}
+                controlClassName='myControlClassName'
+                className='dropdown'
+                options={props.groupTypeDropdown}
+              />
+            </div>
           </div>
           <div>
             <Label>School Grade</Label>
@@ -130,108 +247,185 @@ const CourseRegistrationForm = props => {
           </div>
           <div>
             <Label>Level</Label>
-            <Dropdown
-              value={course.level_id}
-              onChange={e => setCourse({ ...course, level_id: e })}
-              controlClassName='myControlClassName'
-              className='dropdown'
-              options={props.levelDropdown}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderLevel}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Dropdown
+                value={course.level_id}
+                onChange={e => setCourse({ ...course, level_id: e })}
+                controlClassName='myControlClassName'
+                className='dropdown'
+                options={props.levelDropdown}
+              />
+            </div>
           </div>
           <div>
             <Label>Section</Label>
-            <Dropdown
-              value={course.section}
-              onChange={e => setCourse({ ...course, section: e.value })}
-              controlClassName='myControlClassName'
-              className='dropdown'
-              options={section}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderSection}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Dropdown
+                value={course.section}
+                onChange={e => setCourse({ ...course, section: e.value })}
+                controlClassName='myControlClassName'
+                className='dropdown'
+                options={section}
+              />
+            </div>
           </div>
           <div>
             <Label>Course Schedule</Label>
-            <Dropdown
-              value={course.course_schedule_id}
-              onChange={e => setCourse({ ...course, course_schedule_id: e })}
-              controlClassName='myControlClassName'
-              className='dropdown'
-              options={props.courseScheduleDropdown}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderSchedule}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Dropdown
+                value={course.course_schedule_id}
+                onChange={e => setCourse({ ...course, course_schedule_id: e })}
+                controlClassName='myControlClassName'
+                className='dropdown'
+                options={props.courseScheduleDropdown}
+              />
+            </div>
           </div>
           <div>
             <Label>Start Date</Label>
-            <Input
-              type='date'
-              name='start_date'
-              value={course.start_date}
-              onChange={handleChange}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderStartDate}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Input
+                type='date'
+                name='start_date'
+                value={course.start_date}
+                onChange={handleChange}
+              />
+            </div>
           </div>
           <div>
             <Label>End Date</Label>
-            <Input
-              type='date'
-              name='end_date'
-              value={course.end_date}
-              onChange={handleChange}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderEndDate}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Input
+                type='date'
+                name='end_date'
+                value={course.end_date}
+                onChange={handleChange}
+              />
+            </div>
           </div>
           <div>
             <Label>Start Time</Label>
-            <Input
-              type='time'
-              name='start_time'
-              value={course.start_time}
-              onChange={handleChange}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderStartTime}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Input
+                type='time'
+                name='start_time'
+                value={course.start_time}
+                onChange={handleChange}
+              />
+            </div>
           </div>
           <div>
             <Label>End Time</Label>
-            <Input
-              type='time'
-              name='end_time'
-              value={course.end_time}
-              onChange={handleChange}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderEndTime}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Input
+                type='time'
+                name='end_time'
+                value={course.end_time}
+                onChange={handleChange}
+              />
+            </div>
           </div>
           <div>
             <Label>Room</Label>
-            <Dropdown
-              value={course.room_id}
-              onChange={e => setCourse({ ...course, room_id: e })}
-              controlClassName='myControlClassName'
-              className='dropdown'
-              options={props.roomDropdown}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderRoom}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Dropdown
+                value={course.room_id}
+                onChange={e => setCourse({ ...course, room_id: e })}
+                controlClassName='myControlClassName'
+                className='dropdown'
+                options={props.roomDropdown}
+              />
+            </div>
           </div>
           <div>
             <Label>Teacher</Label>
-            <Dropdown
-              value={course.teacher_id}
-              onChange={e => setCourse({ ...course, teacher_id: e })}
-              controlClassName='myControlClassName'
-              className='dropdown'
-              options={props.teacherDropdown}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderTeacher}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Dropdown
+                value={course.teacher_id}
+                onChange={e => setCourse({ ...course, teacher_id: e })}
+                controlClassName='myControlClassName'
+                className='dropdown'
+                options={props.teacherDropdown}
+              />
+            </div>
           </div>
           <div>
             <Label>Hourly Rate</Label>
-            <Input
-              type='text'
-              name='hourly_rate'
-              value={course.hourly_rate}
-              onChange={handleChange}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderHourlyRate}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Input
+                type='text'
+                name='hourly_rate'
+                value={course.hourly_rate}
+                onChange={handleChange}
+              />
+            </div>
           </div>
           <div>
             <Label>Status</Label>
-            <Dropdown
-              value={course.status}
-              onChange={e => setCourse({ ...course, status: e.value })}
-              controlClassName='myControlClassName'
-              className='dropdown'
-              options={status}
-            />
+            <div
+              style={{
+                border: `1px solid ${errorBorderStatus}`,
+                borderRadius: '3px',
+              }}
+            >
+              <Dropdown
+                value={course.status}
+                onChange={e => setCourse({ ...course, status: e.value })}
+                controlClassName='myControlClassName'
+                className='dropdown'
+                options={status}
+              />
+            </div>
           </div>
           <div>
             <Label>Notes</Label>
