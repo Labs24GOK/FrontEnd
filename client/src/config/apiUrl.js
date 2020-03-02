72bb98c99aa799b7354c5a3dfad5cfb29dc2ff6b
David Vazquez <40529534+dave-vazquez@users.noreply.github.com>
2020-03-01 19:33:45 -0500

    Revert "Final Changes"

--- client/src/config/apiUrl.js
+++ client/src/config/apiUrl.js
@@ -2,6 +2,6 @@ const productionURL = `https://speakout-lambda.herokuapp.com`;
 const stagingURL = `https://speakout-lambda-staging.herokuapp.com`;
 const localhost = `http://localhost:4000`;
 
-const API_URL = localhost;
+const API_URL = productionURL;
 
 export default API_URL;
