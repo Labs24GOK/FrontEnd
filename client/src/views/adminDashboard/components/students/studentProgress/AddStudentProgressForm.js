94305bb39f9d165548f78ddaa3114f5dd4bcc091
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-03-30 14:00:35 -0700

    correct errors property from 'cpr' to each input's respective category

--- client/src/views/adminDashboard/components/students/studentProgress/AddStudentProgressForm.js
+++ client/src/views/adminDashboard/components/students/studentProgress/AddStudentProgressForm.js
@@ -36,127 +36,127 @@ const StudentProgressForm = props => {
                     <div>
                         <Label>Speaking Fluency</Label>
                         <div>
-							 <Input type="number" className={errors.speaking_fluency && "input-error"}  name="speaking_fluency" ref={register({required: true})} />
-							    {errors.cpr && 'Speaking Fluency Score is Required'}
+							 <Input type="number" className={errors.speaking_fluency && "input-error"} name="speaking_fluency" ref={register({required: true})} />
+							    {errors.speaking_fluency && 'Speaking Fluency Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Speaking Accuracy</Label>
                         <div>
-							 <Input type="number" className={errors.speaking_accuracy && "input-error"}  name="speaking_accuracy" ref={register({required: true})} />
-							    {errors.cpr && 'Speaking Accuracy Score is Required'}
+							 <Input type="number" className={errors.speaking_accuracy && "input-error"} name="speaking_accuracy" ref={register({required: true})} />
+							    {errors.speaking_accuracy && 'Speaking Accuracy Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Vocabulary</Label>
                         <div>
-							 <Input type="number" className={errors.vocabulary && "input-error"}  name="vocabulary" ref={register({required: true})} />
-							    {errors.cpr && 'Vocabulary Score is Required'}
+							 <Input type="number" className={errors.vocabulary && "input-error"} name="vocabulary" ref={register({required: true})} />
+							    {errors.vocabulary && 'Vocabulary Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Pronunciation</Label>
                         <div>
-							 <Input type="number" className={errors.pronunciation && "input-error"}  name="pronunciation" ref={register({required: true})} />
-							    {errors.cpr && 'Pronunciation Score is Required'}
+							 <Input type="number" className={errors.pronunciation && "input-error"} name="pronunciation" ref={register({required: true})} />
+							    {errors.pronunciation && 'Pronunciation Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Grammar</Label>
                         <div>
-							 <Input type="number" className={errors.grammar && "input-error"}  name="grammar" ref={register({required: true})} />
-							    {errors.cpr && 'Grammar Score is Required'}
+							 <Input type="number" className={errors.grammar && "input-error"} name="grammar" ref={register({required: true})} />
+							    {errors.grammar && 'Grammar Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Listening</Label>
                         <div>
-							 <Input type="number" className={errors.listening && "input-error"}  name="listening" ref={register({required: true})} />
-							    {errors.cpr && 'Listening Score is Required'}
+							 <Input type="number" className={errors.listening && "input-error"} name="listening" ref={register({required: true})} />
+							    {errors.listening && 'Listening Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Writing</Label>
                         <div>
-							 <Input type="number" className={errors.writing && "input-error"}  name="writing" ref={register({required: true})} />
-							    {errors.cpr && 'Writing Score is Required'}
+							 <Input type="number" className={errors.writing && "input-error"} name="writing" ref={register({required: true})} />
+							    {errors.writing && 'Writing Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Reading</Label>
                         <div>
-							 <Input type="number" className={errors.reading && "input-error"}  name="reading" ref={register({required: true})} />
-							    {errors.cpr && 'Reading Score is Required'}
+							 <Input type="number" className={errors.reading && "input-error"} name="reading" ref={register({required: true})} />
+							    {errors.reading && 'Reading Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Interest</Label>
                         <div>
-							 <Input type="number" className={errors.interest && "input-error"}  name="interest" ref={register({required: true})} />
-							    {errors.cpr && 'Interest Score is Required'}
+							 <Input type="number" className={errors.interest && "input-error"} name="interest" ref={register({required: true})} />
+							    {errors.interest && 'Interest Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Participation</Label>
                         <div>
-							 <Input type="number" className={errors.participation && "input-error"}  name="participation" ref={register({required: true})} />
-							    {errors.cpr && 'Participation Score is Required'}
+							 <Input type="number" className={errors.participation && "input-error"} name="participation" ref={register({required: true})} />
+							    {errors.participation && 'Participation Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Submitting Homework</Label>
                         <div>
-							 <Input type="number" className={errors.submitting_homework && "input-error"}  name="submitting_homework" ref={register({required: true})} />
-							    {errors.cpr && 'Submitting Homework Score is Required'}
+							 <Input type="number" className={errors.submitting_homework && "input-error"} name="submitting_homework" ref={register({required: true})} />
+							    {errors.submitting_homework && 'Submitting Homework Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Homework Effort</Label>
                         <div>
-							 <Input type="number" className={errors.homework_effort && "input-error"}  name="homework_effort" ref={register({required: true})} />
-							    {errors.cpr && 'Homework Effort Score is Required'}
+							 <Input type="number" className={errors.homework_effort && "input-error"} name="homework_effort" ref={register({required: true})} />
+							    {errors.homework_effort && 'Homework Effort Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Notes</Label>
                         <div>
-							 <Input type="text" className={errors.notes && "input-error"}  name="notes" ref={register({required: true})} />
+							 <Input type="text" className={errors.notes && "input-error"} name="notes" ref={register({required: true})} />
 							    
                         </div>				
                     </div>
                     <div>
                         <Label>Overall</Label>
                         <div>
-							 <Input type="number" className={errors.overall && "input-error"}  name="overall" ref={register({required: true})} />
-							    {errors.cpr && 'Overall Score is Required'}
+							 <Input type="number" className={errors.overall && "input-error"} name="overall" ref={register({required: true})} />
+							    {errors.overall && 'Overall Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Course ID</Label>
                         <div>
-							 <Input type="number" className={errors.course_id && "input-error"}  name="course_id" ref={register({required: true})} />
-							    {errors.cpr && 'Course ID is Required'}
+							 <Input type="number" className={errors.course_id && "input-error"} name="course_id" ref={register({required: true})} />
+							    {errors.course_id && 'Course ID is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Student ID</Label>
                         <div>
-							 <Input type="number" className={errors.student_id && "input-error"}  name="student_id" ref={register({required: true})} />
-							    {errors.cpr && 'Student ID is Required'}
+							 <Input type="number" className={errors.student_id && "input-error"} name="student_id" ref={register({required: true})} />
+							    {errors.student_id && 'Student ID is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Teacher ID</Label>
                         <div>
-							 <Input type="number" className={errors.teacher_id && "input-error"}  name="teacher_id" ref={register({required: true})} />
-							    {errors.cpr && 'Teacher ID is Required'}
+							 <Input type="number" className={errors.teacher_id && "input-error"} name="teacher_id" ref={register({required: true})} />
+							    {errors.teacher_id && 'Teacher ID is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Report Date</Label>
                         <div>
-							 <Input type="number" className={errors.report_date && "input-error"}  name="report_date" ref={register({required: true})} />
-							    {errors.cpr && 'Report Date is Required'}
+							 <Input type="number" className={errors.report_date && "input-error"} name="report_date" ref={register({required: true})} />
+							    {errors.report_date && 'Report Date is Required'}
                         </div>				
                     </div>
                     </Div>
--- client/src/views/adminDashboard/components/students/studentProgress/EditStudentProgressForm.js
+++ client/src/views/adminDashboard/components/students/studentProgress/EditStudentProgressForm.js
@@ -48,126 +48,126 @@ const EditStudentProgressForm = props => {
                         <Label>Speaking Fluency</Label>
                         <div>
 							 <Input type="number" className={errors.speaking_fluency && "input-error"} name="speaking_fluency" defaultValue={props.progressByStudentId.speaking_fluency} ref={register({required: true})} />
-							    {errors.cpr && 'Speaking Fluency Score is Required'}
+							    {errors.speaking_fluency && 'Speaking Fluency Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Speaking Accuracy</Label>
                         <div>
 							 <Input type="number" className={errors.speaking_accuracy && "input-error"} name="speaking_accuracy" defaultValue={props.progressByStudentId.speaking_accuracy} ref={register({required: true})} />
-							    {errors.cpr && 'Speaking Accuracy Score is Required'}
+							    {errors.speaking_accuracy && 'Speaking Accuracy Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Vocabulary</Label>
                         <div>
 							 <Input type="number" className={errors.vocabulary && "input-error"} name="vocabulary" defaultValue={props.progressByStudentId.vocabulary} ref={register({required: true})} />
-							    {errors.cpr && 'Vocabulary Score is Required'}
+							    {errors.vocabulary && 'Vocabulary Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Pronunciation</Label>
                         <div>
 							 <Input type="number" className={errors.pronunciation && "input-error"} name="pronunciation" defaultValue={props.progressByStudentId.pronunciation} ref={register({required: true})} />
-							    {errors.cpr && 'Pronunciation Score is Required'}
+							    {errors.pronunciation && 'Pronunciation Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Grammar</Label>
                         <div>
 							 <Input type="number" className={errors.grammar && "input-error"} name="grammar"defaultValue={props.progressByStudentId.grammar} ref={register({required: true})} />
-							    {errors.cpr && 'Grammar Score is Required'}
+							    {errors.grammar && 'Grammar Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Listening</Label>
                         <div>
 							 <Input type="number" className={errors.listening && "input-error"} name="listening" defaultValue={props.progressByStudentId.listening} ref={register({required: true})} />
-							    {errors.cpr && 'Listening Score is Required'}
+							    {errors.listening && 'Listening Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Writing</Label>
                         <div>
-							 <Input type="number" className={errors.writing && "input-error"} name="writing"defaultValue={props.progressByStudentId.writing} ref={register({required: true})} />
-							    {errors.cpr && 'Writing Score is Required'}
+							 <Input type="number" className={errors.writing && "input-error"} name="writing" defaultValue={props.progressByStudentId.writing} ref={register({required: true})} />
+							    {errors.writing && 'Writing Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Reading</Label>
                         <div>
-							 <Input type="number" className={errors.reading && "input-error"} name="reading"defaultValue={props.progressByStudentId.reading} ref={register({required: true})} />
-							    {errors.cpr && 'Reading Score is Required'}
+							 <Input type="number" className={errors.reading && "input-error"} name="reading" defaultValue={props.progressByStudentId.reading} ref={register({required: true})} />
+							    {errors.reading && 'Reading Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Interest</Label>
                         <div>
 							 <Input type="number" className={errors.interest && "input-error"} name="interest" defaultValue={props.progressByStudentId.interest} ref={register({required: true})} />
-							    {errors.cpr && 'Interest Score is Required'}
+							    {errors.interest && 'Interest Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Participation</Label>
                         <div>
 							 <Input type="number" className={errors.participation && "input-error"} name="participation" defaultValue={props.progressByStudentId.participation} ref={register({required: true})} />
-							    {errors.cpr && 'Participation Score is Required'}
+							    {errors.participation && 'Participation Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Submitting Homework</Label>
                         <div>
 							 <Input type="number" className={errors.submitting_homework && "input-error"} name="submitting_homework" defaultValue={props.progressByStudentId.submitting_homework} ref={register({required: true})} />
-							    {errors.cpr && 'Submitting Homework Score is Required'}
+							    {errors.submitting_homework && 'Submitting Homework Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Homework Effort</Label>
                         <div>
 							 <Input type="number" className={errors.homework_effort && "input-error"} name="homework_effort" defaultValue={props.progressByStudentId.homework_effort} ref={register({required: true})} />
-							    {errors.cpr && 'Homework Effort Score is Required'}
+							    {errors.homework_effort && 'Homework Effort Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Notes</Label>
                         <div>
-							 <Input type="text" className={errors.notes && "input-error"} name="notes"defaultValue={props.progressByStudentId.notes} ref={register({required: true})} />
+							 <Input type="text" className={errors.notes && "input-error"} name="notes" defaultValue={props.progressByStudentId.notes} ref={register({required: true})} />
 							    
                         </div>				
                     </div>
                     <div>
                         <Label>Overall</Label>
                         <div>
-							 <Input type="number" className={errors.overall && "input-error"} name="overall"defaultValue={props.progressByStudentId.overall} ref={register({required: true})} />
-							    {errors.cpr && 'Overall Score is Required'}
+							 <Input type="number" className={errors.overall && "input-error"} name="overall" defaultValue={props.progressByStudentId.overall} ref={register({required: true})} />
+							    {errors.overall && 'Overall Score is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Course ID</Label>
                         <div>
 							 <Input type="number" className={errors.course_id && "input-error"} name="course_id" defaultValue={props.progressByStudentId.course_id} ref={register({required: true})} />
-							    {errors.cpr && 'Course ID is Required'}
+							    {errors.course_id && 'Course ID is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Student ID</Label>
                         <div>
 							 <Input type="number" className={errors.student_id && "input-error"} name="student_id" defaultValue={props.student_id} ref={register({required: true})} />
-							    {errors.cpr && 'Student ID is Required'}
+							    {errors.student_id && 'Student ID is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Teacher ID</Label>
                         <div>
 							 <Input type="number" className={errors.teacher_id && "input-error"} name="teacher_id" defaultValue={props.progressByStudentId.teacher_id} ref={register({required: true})} />
-							    {errors.cpr && 'Teacher ID is Required'}
+							    {errors.teacher_id && 'Teacher ID is Required'}
                         </div>				
                     </div>
                     <div>
                         <Label>Report Date</Label>
                         <div>
 							 <Input type="date" className={errors.report_date && "input-error"} name="report_date" defaultValue={reportDate} ref={register({required: true})} />
-							    {errors.cpr && 'Report Date is Required'}
+							    {errors.report_date && 'Report Date is Required'}
                         </div>				
                     </div>
                     </Div>
