2466e675ee4feabbfcffcdbe413ddb783808ec83
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-03-31 10:33:00 -0700

    change login form to user react-hook-form

--- client/src/authentication/Login.js
+++ client/src/authentication/Login.js
@@ -3,53 +3,15 @@ import { Redirect, Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import { connect } from "react-redux";
 import { logIn, loggedIn } from "../actions/authenticationActions.js";
-// import NavBar from "../views/landingPage/components/header/NavBar";  -> labs18 comment
-// import Footer from "../views/footer/Footer";  -> labs18 comment
+import { useForm } from 'react-hook-form';
 import LoginGroup from "../assets/LoginGroup.png";
 import "./login.scss";
 
 function Login(props) {
-  const [user, setUser] = useState({
-    username: "",
-    password: ""
-  });
 
-  const [formValid, setFormValid] = useState(true);
-  const [modal, setModal] = useState(false);
-
-  const [email, setEmail] = useState("");
-
-  const handleChange = e => {
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value
-    });
-    e.target.focus();
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (user.username.length && user.password.length) {
+	const { register, errors, handleSubmit } = useForm();
+  const formSubmit = user => {
       props.logIn(user, props.history);
-
-      setUser({
-        username: "",
-        password: ""
-      });
-    } else {
-      setFormValid(false);
-    }
-  };
-
-  const handleEmailChange = e => {
-    setEmail(e.target.value);
-  };
-
-  const handleEmailSubmit = e => {
-    e.preventDefault();
-    // Add logic to communicate with backend to send email -> labs18 comment
-    setEmail("");
-    setModal(false);
   };
 
   if (props.state.authenticationReducer.user.authenticated) {
@@ -57,58 +19,22 @@ function Login(props) {
   } else {
     return (
       <div className="login">
-        {/* <NavBar /> */}
         <div className="login-body">
           <div className="img-container">
             <img src={LoginGroup} />
           </div>
           <div className="form-container">
             <h1>Welcome back!</h1>
-            <form onSubmit={handleSubmit} method="POST">
+            <form onSubmit={handleSubmit(formSubmit)}>
+              
               <fieldset>
-                <label htmlFor="email">Email</label>
-                <input
-                  style={{ marginBottom: "15px" }}
-                  type="text"
-                  name="username"
-                  placeholder="Username"
-                  onChange={handleChange}
-                  value={user.username}
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                  style={{ marginBottom: "15px" }}
-                  className="input-class"
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  onChange={handleChange}
-                  value={user.password}
-                />
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" placeholder="Username" ref={register({required: true, minLength: 1})} />
 
-                {/* <span
-                  style={{ textDecoration: "underline", cursor: "pointer" }}
-                  onClick={() => {
-                    setModal(true);
-                  }}
-                >
-                  Forgot password?
-                </span>  -> labs18 comment */ }
-                <button
-                  type="submit"
-                  style={{
-                    background: `${
-                      user.username.length && user.password.length
-                        ? "#c73642"
-                        : "#ffffff"
-                    }`,
-                    color: `${
-                      user.username.length && user.password.length
-                        ? "#ffffff"
-                        : "#c73642"
-                    }`
-                  }}
-                >
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" placeholder="Password" ref={register({required: true, minLength: 1})} />
+
+                <button type="submit" className={(errors.username || errors.password || props.state.authenticationReducer.logIn.error) ? "disabled" : ""}>
                   Sign in
                 </button>
               </fieldset>
@@ -119,41 +45,19 @@ function Login(props) {
                 </Link>
               </div>
               <div>
-                {!formValid && <p>Username and Password are required.</p>}
-                {props.state.authenticationReducer.logIn.error && (
-                  <p>Login credentials incorrect.</p>
-                )}
+                {(errors.username || errors.password) && <p>Username and Password are required.</p> && console.log(errors)}
+                {props.state.authenticationReducer.logIn.error && <p>Login credentials incorrect.</p>}
               </div>
             </form>
-            {modal ? (
-              <form onSubmit={handleEmailSubmit}>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email address"
-                  onChange={handleEmailChange}
-                  value={email}
-                />
-                <button>Submit</button>
-              </form>
-            ) : null}
           </div>
         </div>
-        {/* <Footer />  -> labs18 comment */}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return {
-    state: state
-  };
+  return { state: state };
 };
 
-export default withRouter(
-  connect(
-    mapStateToProps,
-    { logIn, loggedIn }
-  )(Login)
-);
+export default withRouter(connect( mapStateToProps, { logIn, loggedIn } )(Login));
