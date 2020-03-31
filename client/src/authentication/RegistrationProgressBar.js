c74c57aea862718d6b1951c9e9d196f8f3b40588
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-03-31 14:52:57 -0700

    destructure 'step' from props in RegistrationProgressBar

--- client/src/authentication/RegistrationProgressBar.js
+++ client/src/authentication/RegistrationProgressBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const RegistrationProgressBar = (step) => {
+const RegistrationProgressBar = ({step}) => {
     return (
         <>
             <div className='horiz-line' />
