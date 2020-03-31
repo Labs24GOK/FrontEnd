2c7c85e9583771e26ade8440a3f51d8499399f33
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-03-31 16:00:41 -0700

    move hardcoded CSS from success-message div to register.scss file

--- client/src/authentication/RegistrationSuccessMessage.js
+++ client/src/authentication/RegistrationSuccessMessage.js
@@ -4,16 +4,7 @@ import { Link } from 'react-router-dom';
 const RegistrationSuccessMessage = ({studentName}) => {
 
     return (
-            <div className='success-message'
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                textAlign: 'left',
-                margin: '40px',
-                alignItems: 'center',
-                height: 'calc(100vh - 282px)',
-            }}
-            >
+            <div className='success-message'>
                 <h1>Congratulations, {studentName}!</h1>
                 <h2>You've registered for Speak Out. Thank you!</h2>
                 <Link to='/login' className='button'>
