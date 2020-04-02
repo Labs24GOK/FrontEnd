f86dda6347abd4b33dbd19079e8af8c23086c9fa
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-02 14:49:50 -0700

    add input-error class to ParentForm inputs

--- client/src/views/adminDashboard/components/parents/ParentForm.js
+++ client/src/views/adminDashboard/components/parents/ParentForm.js
@@ -6,6 +6,8 @@ import 'react-dropdown/style.css';
 import { useForm } from 'react-hook-form';
 import { FormWrap, Input, CancelButton, SaveButton, ButtonDiv, Div, FormSet, Label} from '../mainStyle/styledComponent';
 
+import '../mainStyle/mainTable.scss';
+
 const ParentForm = props => {
     
     const parent = props.parentById;
@@ -26,56 +28,56 @@ const ParentForm = props => {
                     <div>
                         <Label>Father Name</Label>
                         <div>
-                            <Input type='text' name='father_name' placeholder='Father Name' defaultValue={parent.father_name} ref={register({required: true})} />
+                            <Input type='text' name='father_name' placeholder='Father Name' defaultValue={parent.father_name} className={errors.father_name && "input-error"} ref={register({required: true})} />
                             {errors.father_name && errors.father_name.type === "required" && 'Father Name is Required'}
                         </div>
                     </div>
                     <div>
                         <Label>Mother Name</Label>
                         <div>
-                            <Input type='text' name='mother_name' placeholder='Mother Name' defaultValue={parent.mother_name} ref={register({required: true})} />
+                            <Input type='text' name='mother_name' placeholder='Mother Name' defaultValue={parent.mother_name} className={errors.mother_name && "input-error"} ref={register({required: true})} />
                             {errors.mother_name && errors.mother_name.type === "required" && 'Mother Name is Required'}
                         </div>
                     </div>
                     <div>
                         <Label>Primary Telephone</Label>
                         <div>
-                            <Input type='text' name='primary_telephone' placeholder='Primary Telephone' defaultValue={parent.primary_telephone} ref={register({required: true})} />
+                            <Input type='text' name='primary_telephone' placeholder='Primary Telephone' defaultValue={parent.primary_telephone} className={errors.primary_telephone && "input-error"} ref={register({required: true})} />
                             {errors.primary_telephone && errors.primary_telephone.type === "required" && 'Primary Telephone is Required'}
                         </div>
                     </div>
                     <div>
                         <Label>Secondary Telephone</Label>
                         <div>
-                            <Input type='text' name='secondary_telephone' placeholder='Secondary Telephone' defaultValue={parent.secondary_telephone} ref={register({required: true})} />
+                            <Input type='text' name='secondary_telephone' placeholder='Secondary Telephone' defaultValue={parent.secondary_telephone} className={errors.secondary_telephone && "input-error"} ref={register({required: true})} />
                             {errors.secondary_telephone && errors.secondary_telephone.type === "required" && 'Secondary Telephone is Required'}
                         </div>
                     </div>
                     <div>
                         <Label>Block Code</Label>
                         <div>
-                            <Input type='text' name='block_code' placeholder='Block Code' defaultValue={parent.block_code} ref={register({required: true})} />
+                            <Input type='text' name='block_code' placeholder='Block Code' defaultValue={parent.block_code} className={errors.block_code && "input-error"} ref={register({required: true})} />
                             {errors.block_code && errors.block_code.type === "required" && 'Block Code is Required'}
                         </div>
                     </div>
                     <div>
                         <Label>Building</Label>
                         <div>
-                            <Input type='text' name='building' placeholder='Building' defaultValue={parent.building} ref={register({required: true})} />
+                            <Input type='text' name='building' placeholder='Building' defaultValue={parent.building} className={errors.building && "input-error"} ref={register({required: true})} />
                             {errors.building && errors.building.type === "required" && 'Building is Required'}
                         </div>
                     </div>
                     <div>
                         <Label>Flat</Label>
                         <div>
-                            <Input type='text' name='flat' placeholder='Flat' defaultValue={parent.flat} ref={register({required: true})} />
+                            <Input type='text' name='flat' placeholder='Flat' defaultValue={parent.flat} className={errors.flat && "input-error"} ref={register({required: true})} />
                             {errors.flat && errors.flat.type === "required" && 'Flat is Required'}
                         </div>
                     </div>
                     <div>
                         <Label>Road</Label>
                         <div>
-                            <Input type='text' name='road' placeholder='Road' defaultValue={parent.road} ref={register({required: true})} />
+                            <Input type='text' name='road' placeholder='Road' defaultValue={parent.road} className={errors.road && "input-error"} ref={register({required: true})} />
                             {errors.road && errors.road.type === "required" && 'Road is Required'}
                         </div>
                     </div>                 
