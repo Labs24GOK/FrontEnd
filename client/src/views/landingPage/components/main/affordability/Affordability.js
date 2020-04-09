d6d1aa45ba56fe28d10992a4ec4220214e279916
Hiter Harris <hiterharris@comcast.net>
2020-04-09 13:28:49 -0400

    updated text on page one

--- client/src/views/landingPage/components/main/affordability/Affordability.js
+++ client/src/views/landingPage/components/main/affordability/Affordability.js
@@ -11,7 +11,7 @@ function Affordability() {
           <div></div>
           <div>
             <h2>Affordability</h2>
-            <p>In association with the Bani Jamra Charity, Speak Out brings quality, affordable language learning to the community.</p>
+            <p>From its conception, the Garden of Knowledge was about standing shoulder to shoulder with local comminities. As such we have made our rates one of the lowest.</p>
             <Link to="/registration-information" className="button">Learn More</Link>
           </div>
         </div>
