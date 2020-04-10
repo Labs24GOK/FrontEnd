fcf0f7362b5c0639b3f9766c41136ed93322c1b7
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-10 14:30:36 -0700

    create initial placeholder mobile view components

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -60,6 +60,7 @@ import Primary from '../views/landingPage/components/courseStructure/Primary';
 import MiddleAndSecondary from '../views/landingPage/components/courseStructure/MiddleAndSecondary';
 import Register from '../authentication/Register';
 
+import MarketingMobile from "../views/marketingMobile/MarketingMobile";
 
 function Routes(props) {
   useEffect(() => {
@@ -129,6 +130,10 @@ function Routes(props) {
             <CourseStructureHeader />
             <MiddleAndSecondary />
             <Footer />
+          </>}/> 
+        <Route exact path="/mobile" render={() => 
+          <>
+            <MarketingMobile />
           </>}/> 
       </Switch>
     </div>
