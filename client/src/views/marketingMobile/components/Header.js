5442ded7579a93d65e8c013010a84cbea61875f9
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-10 16:15:03 -0700

    add social media icons to footer
    Co-Authored-By: Richard <RichardMachado>
    Co-Authored-By: Ammon <AmmonMatthews>
    Co-Authored-By: Hiter <hiterharris>

--- client/src/views/marketingMobile/components/Header.js
+++ client/src/views/marketingMobile/components/Header.js
@@ -15,13 +15,15 @@ function Header() {
                 <img src={LogoTree} alt="The Garden of Knowledge" />
                 <img src={HamburgerMenu} alt="Mobile Menu Icon" />
             </header>
-            <div className="hamburger-menu">
+
+
+            {/* <div className="hamburger-menu">
                 <NavLink to='/course-structure' className="hamburger-link">Course Structure</NavLink>
                 <NavLink to='/registration-information' className="hamburger-link"> Registration Information</NavLink>
                 <NavLink to='/about-us' className="hamburger-link">About Us</NavLink>
                 <NavLink to='/contact-us' className="hamburger-link">Contact Us</NavLink>
                 <NavLink to='/login' className="hamburger-link">Sign In</NavLink>
-            </div>
+            </div> */}
         </>
     )
 }
