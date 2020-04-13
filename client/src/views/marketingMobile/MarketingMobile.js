80026ddeb6935854d5025ab3c52a47702eaf938c
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-04-13 15:56:41 -0600

    home page scafolded

--- client/src/views/marketingMobile/MarketingMobile.js
+++ client/src/views/marketingMobile/MarketingMobile.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+
+import './marketing.scss'
+
 
 function MarketingMobile() {
     return (
         <div>
             <Header />
             <div className="content">
-                
+                <Home />
             </div>
             <Footer />
         </div>
