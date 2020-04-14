4ce958d37c7d9a511ff95aa2e87386e8d2369a20
Hiter Harris <hiterharris@comcast.net>
2020-04-14 17:53:37 -0400

    added new images, rearranged layout

--- client/src/views/marketingMobile/components/Registration.js
+++ client/src/views/marketingMobile/components/Registration.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import PlacementTest from '../../../assets/placement-test.jpg';
+import Terms from '../../../assets/terms.jpg';
+import Fees from '../../../assets/fees.jpg';
+
 import './Registration.scss';
-import Header from './Header';
-import Footer from './Footer';
 
 function About() {
-
     return (
         <div className='registration-container'>
-            {/* <Header />s */}
             <h1>Registration Information</h1>
 
-            <section className="top-content">
-                <div></div>
-                <div className="placement-test">
+            <section className="placement-test">
+                <img src={PlacementTest} />
+
+                <div>
                     <h2>Placement Test</h2>
                     <p>This can be taken online or at the center and will help us find the student's level when taking a language course.</p>
                 </div>
@@ -23,15 +24,19 @@ function About() {
                     <h2>Terms and Conditions</h2>
                     <p>A parent will be required to sign a document containing course information and student behaviour guide, as well as commiting them to finish a 3 month section once they have started classes.</p>
                 </div>
-                <div></div>
+                <img src={Terms} />
             </section>
 
             <section className="fees">
+            <img src={Fees} />
+            <div>
                 <h2>Fees</h2>
                 <p>Kindergarten: 122BHD</p>
                 <p>Primary: 122BHD</p>
                 <p>Intermediate and Secondary: 127BHD</p>
                 <p>Cost includes registration, books, stationery, and 24 classes.</p>
+            </div>
+
             </section>
         </div>
     )
