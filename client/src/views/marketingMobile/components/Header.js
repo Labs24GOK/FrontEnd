7f0250147d9fc01fe84531cb75c34f5ff04642f8
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-14 10:41:39 -0700

    add onClick toggle to hamburger menu and add hamburger menu icon

--- client/src/views/marketingMobile/components/Header.js
+++ client/src/views/marketingMobile/components/Header.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import LogoTree from "../../../assets/Garden_tree_only.png";
-import HamburgerMenu from "../../../assets/Home.png";
+import HamburgerMenu from "../../../assets/hamburger_menu_icon.png";
 
 import "./Header.scss";
 
 function Header() {
 
-    
+    const [menuDisplayStatus, setMenuDisplayStatus] = useState("hamburger-menu-hidden");
+
+    const toggleMenu = () => {
+        setMenuDisplayStatus(menuDisplayStatus === "hamburger-menu-hidden" ? "" : "hamburger-menu-hidden");
+    }
 
     return (
         <>
             <header className="mobile">
                 <img src={LogoTree} alt="The Garden of Knowledge" />
-                <img src={HamburgerMenu} alt="Mobile Menu Icon" />
+                <img src={HamburgerMenu} alt="Mobile Menu Icon" className="hamburger-menu-icon" onClick={toggleMenu} />
             </header>
 
-            <div className="hamburger-menu hidden">
-                <NavLink to='/' className="hamburger-link">Home</NavLink>
-                <NavLink to='/courses' className="hamburger-link">Courses</NavLink>
-                <NavLink to='/course_structure' className="hamburger-link">Course Structure</NavLink>
-                <NavLink to='/registration' className="hamburger-link"> Registration Information</NavLink>
-                <NavLink to='/about' className="hamburger-link">About Us</NavLink>
-                <NavLink to='/contact' className="hamburger-link">Contact Us</NavLink>
+            <div className={"hamburger-menu " + menuDisplayStatus}>
+                <NavLink to='/mobile' className="hamburger-link">Home</NavLink>
+                <NavLink to='/mobile/courses' className="hamburger-link">Courses</NavLink>
+                <NavLink to='/mobile/course_structure' className="hamburger-link">Course Structure</NavLink>
+                <NavLink to='/mobile/registration' className="hamburger-link"> Registration Information</NavLink>
+                <NavLink to='/mobile/about' className="hamburger-link">About Us</NavLink>
+                <NavLink to='/mobile/contact' className="hamburger-link">Contact Us</NavLink>
                 <NavLink to='/login' className="hamburger-link">Sign In</NavLink>
             </div>
         </>
