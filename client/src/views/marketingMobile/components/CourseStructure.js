49a145dece6755888edaadd4ef3db3af442a6913
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-04-15 10:22:25 -0600

    editing content

--- client/src/views/marketingMobile/components/CourseStructure.js
+++ client/src/views/marketingMobile/components/CourseStructure.js
@@ -7,7 +7,7 @@ function CourseStructure() {
 
     return (
         <>
-            <h1>Course Structure</h1>
+            <h1>Schedules</h1>
             
             <section>
                 <div className="two-column">
@@ -20,25 +20,10 @@ function CourseStructure() {
             </section>
 
             <section>
-                <h2 className="section-even">Success</h2>
-                <p>At the end of each section, the student will submit a final exam and based on the results of the exam is determined the appropriate procedure results are as follows:</p>
-                
-                <div className="two-column">
-                    <div className="two-column-left">
-                        <h3>Successful and Above</h3>
-                        <p>The student will continue at the same level but the next section, unless in the third section of the course in this case the student will go to the next level.</p>
-                    </div>
-                   
-                </div>
-
-                <p>* In case the student excels in his current level with the recommendation of the teacher may take the student to the next level.</p>
-            </section>
-
-            <section>
-                <h2 className="section-odd">Schedules</h2>
+                <h2 className="section-odd">Overall Course Structure</h2>
                 <p>Where possible we will try to acommodate your needs when issuing schedules. However, this may not always be possible considering levels and other students who are already scheduled for the same group.</p>
 
-                <h3>Overall Course Structure</h3>
+                
 
                 <section>
                     <h4>What does a level look like?</h4>
