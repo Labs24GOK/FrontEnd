eb513a592c8c741021b32a777410f83091ab1f64
AmmonMatthews <ammonmatthews@gmail.com>
2020-04-17 15:49:47 -0600

    desktop styles

--- client/src/views/marketing/components/AboutAndContact.js
+++ client/src/views/marketing/components/AboutAndContact.js
@@ -7,7 +7,7 @@ import Facebook from '../../../assets/facebook-peacock.png'
 import Twitter from '../../../assets/twitter-peacock.png'
 import phone from '../../../assets/phone-peacock.png'
 
-import children from '../../../assets/children.jpg'
+import children from '../../../assets/about-pic.jpg'
 
 function AboutAndContact() {
 
@@ -17,22 +17,22 @@ function AboutAndContact() {
             
             <div className="wrapper">
                 <section className="about-container" >
-                <h1>About Us</h1>
-            <section className="about-content">
-                <p>The Garden of Knowledge founded originally as a charity program in 2014, has always had the local comminity at heart. We have a passion for providing the highest standards to students and parents, and a positive, productive work environment for our staff.</p>
-            </section>
-                <div className="two-column">
-                    <div className="two-column-left">
-                        <h3>Our Vision</h3>
-                        <p>Making nationwide interactive language learning a possibility for all.</p>
-                    </div>
-                    <div className="two-column-right">
-                        <h3>Our Mission</h3>
-                        <p>We provide the highest standards of language learning, adapting to people's wants and needs. Our students will have a measurable output, both in the center and at school or work.</p>
-                    </div>
+                    <h1>About Us</h1>
+                    <section className="about-content">
+                        <p>The Garden of Knowledge founded originally as a charity program in 2014, has always had the local comminity at heart. We have a passion for providing the highest standards to students and parents, and a positive, productive work environment for our staff.</p>
+                    </section>
+                    <div className="two-column">
+                        <div className="two-column-left">
+                            <h3>Our Vision</h3>
+                            <p>Making nationwide interactive language learning a possibility for all.</p>
+                        </div>
+                        <div className="two-column-right">
+                            <h3>Our Mission</h3>
+                            <p>We provide the highest standards of language learning, adapting to people's wants and needs. Our students will have a measurable output, both in the center and at school or work.</p>
+                        </div>
                     </div>
                     <img className="children" src={children} />
-                    </section>
+                </section>
                  
                     
          
