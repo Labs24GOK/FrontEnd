ebe5f0bb6d9aa7747c4cf0e512706a2a81f41510
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-20 19:50:07 -0700

    add user_type='parent'  property and start formSubmit logic

--- client/src/authentication/Register.js
+++ client/src/authentication/Register.js
@@ -3,16 +3,27 @@ import { Redirect, Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import { connect } from "react-redux";
 import { logIn, loggedIn } from "../actions/authenticationActions.js";
+import { familyRegister } from '../actions/registrationActions';
 import { useForm } from 'react-hook-form';
 import "./loginAndRegister.scss";
 
 function Register(props) {
 
 	const { register, errors, handleSubmit } = useForm();
-  const formSubmit = user => {
+  const formSubmit = async user => {
 
-      user.type = "parent";
-      props.logIn(user, props.history);
+      // only parents will register through this form (no admin or staff)
+      // can change "parent" to another category later
+      user.user_type = "parent";
+      
+      // use old familyRegister action creator to register (for now)
+      await props.familyRegister({user: user, family: null}, props.history);
+
+      // use logIn action creator to log in. Only send username and password data.
+      await props.logIn({username: user.username, password: user.password}, props.history);
+
+      // unfinished; allowed existing accounts to be accessed to log in
+      // can't log in with newly created account (even though 201 is returned).
   };
 
   if (props.state.authenticationReducer.user.authenticated) {
@@ -26,16 +37,16 @@ function Register(props) {
               
               <fieldset>
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" ref={register({required: true, minLength: 1})} />
+                <input type="text" name="name" ref={register({required: true, minLength: 1})} defaultValue="New User"/>
                 
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" ref={register({required: true, minLength: 1})} />
+                <input type="email" name="email" ref={register({required: true, minLength: 1})}  defaultValue="user@gmail.com"/>
 
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username" ref={register({required: true, minLength: 1})} />
+                <input type="text" name="username" ref={register({required: true, minLength: 1})}  defaultValue={Date.now()}/>
 
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" ref={register({required: true, minLength: 1})} />
+                <input type="password" name="password" ref={register({required: true, minLength: 1})} defaultValue="pass" />
 
                 <button type="submit" className={errors.name ? "disabled" : ""}>
                   Register
@@ -56,4 +67,4 @@ const mapStateToProps = state => {
   return { state: state };
 };
 
-export default withRouter(connect( mapStateToProps, { logIn, loggedIn } )(Register));
+export default withRouter(connect( mapStateToProps, { logIn, loggedIn, familyRegister } )(Register));
