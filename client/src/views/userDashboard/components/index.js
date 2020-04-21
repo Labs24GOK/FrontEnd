5b45c13194c0870aafda79c65eff2d371dff7bf6
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-20 23:30:40 -0700

    add user dashboard header and footer

--- client/src/views/userDashboard/components/index.js
+++ client/src/views/userDashboard/components/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UserDashboardHeader from "./UserDashboardHeader";
+import Footer from "../../marketing/components/Footer";
+
+import "../../marketing/marketing.scss";
+import "../userDashboard.scss";
+
 function Index() {
-  return (<h1>The parent dashboard will be redesigned according to the UX hi-fidelity mockup.</h1>)
+  return (
+    <div className="content">
+      <UserDashboardHeader />
+      <h1>The parent dashboard will be redesigned according to the UX hi-fidelity mockup.</h1>
+      <Footer />
+    </div>
+  )
 }
 
 export default Index;
