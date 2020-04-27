44675c75dfad1dc3e2edbd392132fd658f534d9e
Hiter Harris <hiterharris@comcast.net>
2020-04-27 12:03:55 -0400

    added === to API URL

--- client/src/config/apiUrl.js
+++ client/src/config/apiUrl.js
@@ -3,6 +3,6 @@ const stagingURL = `https://speakout-lambda-staging.herokuapp.com`;
 const localhost = `http://localhost:4000`;
 
 
-const API_URL = (process.env.REACT_APP_NODE_ENV = 'production') ? productionURL : localhost;
+const API_URL = (process.env.REACT_APP_NODE_ENV === 'production') ? productionURL : localhost;
 
 export default API_URL;
