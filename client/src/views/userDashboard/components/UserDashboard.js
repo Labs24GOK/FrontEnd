d605733c9838c34d5c86dbf3e8024eebd8406992
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-27 15:07:52 -0700

    get role from user token instead of localStorage

--- client/src/views/userDashboard/components/UserDashboard.js
+++ client/src/views/userDashboard/components/UserDashboard.js
@@ -6,72 +6,56 @@ import StudentCourseCard from "./StudentCourseCard";
 
 
 import { getStudentsInFamily } from "../getStudentsinFamily";
+import { getMessagesForUser } from "../getMessagesForUser";
 
 function UserDashboard() {
 
     const [userData, setUserData] = useState({});
     const [displayAddStudentModal, setDisplayAddStudentModal] = useState(false);
     
-    useEffect(async () => {
+    // let userID, name;
 
-        // Get userID from JWT
-        let token = localStorage.getItem("token");
-        let tokenData = JSON.parse(atob(token.split('.')[1]));;
+    // Get userID from JWT
+    let token = localStorage.getItem("token");
+    let tokenData = JSON.parse(atob(token.split('.')[1]));;
 
-        let userID = tokenData.subject;
-        let name = tokenData.name;
+    console.log("token data contains:", tokenData);
+
+    let userID = tokenData.subject;
+    let name = tokenData.name;
+
+    // useEffect(async () => {
 
         // retrieve list of students associated with this account (serach by user ID)
-        let students = await getStudentsInFamily(userID);
-        // let students = [];
-        // setStudents(studentArray);
-
+        // let students = await getStudentsInFamily(userID);
+        let students = [];
+        
         // determine which messages to display to user upon login
-        let messages = [];
-
-        // if (students && students.length > 0)
-        //     {
-        //         let issues = 0;
-
-        //         students.forEach(student => {
-
-        //             if (!student.courses)
-        //                 {
-        //                     issues += 1;
-        //                     messages.push(student.first_name + " has not registered for any courses yet.");
-        //                 }
-        //         })
-
-        //         if (issues === 0)
-        //             { messages.push("You're all up to date."); }
-        //     }
-        // else
-        //     {
-        //         messages.push("You don't have any students registered with us yet.")
-        //     }
+        let messages = getMessagesForUser(students);
+        // let messages = [];
 
         // store all info into state variable
-        setUserData({name, userID, messages, students});
+        // setUserData({messages, students});
         
-    }, []);
+    // }, []);
 
     // if userData hasn't loaded yet, return a loading message/icon
     if (Object.keys(userData).length === 0)
         {
             return <h2>Loading...</h2>;
         }
-    else if (!userData.userID)
+    else if (!userID)
         {
             return <h2>Invalid user ID</h2>;
         }
 
     return (
         <div className="userDashboard content">
-            <h1>Welcome, {userData.name}.</h1>
+            <h1>Welcome, {name}.</h1>
             
             {/* <MessageBox messages={userData.messages} />
             <button className="addStudent" onClick={() => setDisplayAddStudentModal(true)}>+ Add a Student</button>
-            <AddStudentModal displayModal={displayAddStudentModal} setDisplayAddStudentModal={setDisplayAddStudentModal} userID={userData.userID} />
+            <AddStudentModal displayModal={displayAddStudentModal} setDisplayAddStudentModal={setDisplayAddStudentModal} userID={userID} />
             {console.log("what's in userData?", userData)}
             {userData.students.map((student, id) => <StudentCourseCard student={student} />)} */}
 
