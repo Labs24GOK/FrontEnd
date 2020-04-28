252e680e7ccdc2b57ee248c31edeb06845827c2b
Hiter Harris <hiterharris@comcast.net>
2020-04-28 16:45:34 -0400

    .

--- client/src/actions/authenticationActions.js
+++ client/src/actions/authenticationActions.js
@@ -7,34 +7,6 @@ export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
-<<<<<<< HEAD
-export const LOGOUT_START = 'LOGOUT_START';
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
-export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
-
-export const loggedIn = (history, location) => {
-	return dispatch => {
-		dispatch({ type: LOGGEDIN_START });
-
-		axios
-			.get(`${API_URL}/user`)
-			.then(res => {
-				dispatch({ type: LOGGEDIN_SUCCESS, payload: res.data });
-				if (!res.data.authenticated && location.pathname === '/dashboard') {
-					history.push('/login');
-				} else if (res.data.authenticated) {
-					history.push('/dashboard');
-				}
-			})
-			.catch(err => {
-				let wrongCredentials = true;
-				dispatch({ type: LOGGEDIN_FAILURE, payload: wrongCredentials });
-			});
-	};
-};
-
-=======
->>>>>>> 44675c75dfad1dc3e2edbd392132fd658f534d9e
 export const logIn = (user, history) => {
 	return dispatch => {
 		dispatch({ type: LOGIN_START });
