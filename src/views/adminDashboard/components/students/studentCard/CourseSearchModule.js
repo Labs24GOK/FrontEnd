41642652d222cea6c722c0b698ee8b0e666fd4db
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-05-20 15:02:43 -0700

    antd update requres key in tables. fix courseData

--- src/views/adminDashboard/components/students/studentCard/CourseSearchModule.js
+++ src/views/adminDashboard/components/students/studentCard/CourseSearchModule.js
@@ -222,9 +222,16 @@ const CourseSearchModule = props => {
     props.setModalVisible({ visible: false });
   };
 
-  const courseData = props.courseList.sort((a, b) => {
-    return b.id - a.id;
-  });
+  const courseData = (courses) => {
+    const list = courses.sort((a, b) => {
+      return b.id - a.id;
+    });
+    const courseList = list.map((course, index) => {
+      console.log(course)
+      return { ...course, key: index }
+    })
+    return courseList;
+  }
 
   const rowSelection = {
     type: 'radio',
@@ -261,13 +268,14 @@ const CourseSearchModule = props => {
               ]}
             >
               <Table
-                dataSource={courseData}
+                dataSource={courseData(props.courseList)}
                 columns={tableColumns}
                 pagination={true}
                 rowSelection={rowSelection}
               />
             </Modal>
           </div>}
+          {console.log(courseData)}
     </div>
   );
 };
