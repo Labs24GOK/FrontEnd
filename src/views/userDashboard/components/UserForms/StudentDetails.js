c3b8c1e128851044962cac634573316c8add0aaa
Alec Dye <alecdye@gmail.com>
2020-05-21 15:05:16 -0700

    Style adjusting layout of DoB/gender and grade level

--- src/views/userDashboard/components/UserForms/StudentDetails.js
+++ src/views/userDashboard/components/UserForms/StudentDetails.js
@@ -8,6 +8,8 @@ import {
   DatePicker,
   Tooltip,
   Typography,
+  Row,
+  Col,
 } from 'antd';
 import moment from 'moment-timezone';
 import { QuestionCircleOutlined } from '@ant-design/icons';
@@ -22,8 +24,8 @@ const StudentDetails = props => {
       <Form layout={'vertical'} onChange={handleChange}>
         <Title level={3}>Student Details</Title>
         <Form.Item
-          name='first_name'
-          label='Full Name'
+          name="first_name"
+          label="Full Name"
           rules={[
             {
               required: true,
@@ -34,11 +36,11 @@ const StudentDetails = props => {
           <Input />
         </Form.Item>
         <Form.Item
-          name='additional_names'
+          name="additional_names"
           label={
             <span>
               Preferred Name
-              <Tooltip title='What do you want others to call you?'>
+              <Tooltip title="What do you want others to call you?">
                 {' '}
                 <QuestionCircleOutlined />
               </Tooltip>
@@ -48,8 +50,8 @@ const StudentDetails = props => {
           <Input />
         </Form.Item>
         <Form.Item
-          name='cpr'
-          label='Government ID'
+          name="cpr"
+          label="Government ID"
           rules={[
             {
               required: true,
@@ -59,39 +61,48 @@ const StudentDetails = props => {
         >
           <Input />
         </Form.Item>
+
+        <Row justify="center">
+          <Col>
+            <Form.Item
+              name="birthdate"
+              label="Date of Birth"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter student's date of birth",
+                },
+              ]}
+            >
+              <DatePicker
+                defaultValue={moment('01/04/2014', dateFormat)}
+                format={dateFormat}
+                style={{ width: 120 }}
+              />
+            </Form.Item>
+          </Col>
+          <Col>
+            <Form.Item
+              name="gender"
+              label="Gender"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter student's gender",
+                },
+              ]}
+            >
+              <Select defaultValue="Male" style={{ width: 80 }}>
+                <Option value="M">Male</Option>
+                <Option value="F">Female</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+
         <Form.Item
-          name='birthdate'
-          label='Date of Birth'
-          rules={[
-            {
-              required: true,
-              message: "Please enter student's date of birth",
-            },
-          ]}
-        >
-          <DatePicker
-            defaultValue={moment('01/04/2014', dateFormat)}
-            format={dateFormat}
-          />
-        </Form.Item>
-        <Form.Item
-          name='gender'
-          label='Gender'
-          rules={[
-            {
-              required: true,
-              message: "Please enter student's gender",
-            },
-          ]}
-        >
-          <Select defaultValue='Male' style={{ width: 100 }}>
-            <Option value='M'>Male</Option>
-            <Option value='F'>Female</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name='mobile_telephone'
-          label='Phone'
+          name="phone_number"
+          label="Phone"
           rules={[
             {
               required: true,
@@ -99,42 +110,41 @@ const StudentDetails = props => {
             },
           ]}
         >
-          <Input
-            placeholder={'332-32-1234'}
-          />
+          <Input placeholder={'332-32-1234'} />
         </Form.Item>
-        <Form.Item name='email' label='Email'>
+        <Form.Item name="email" label="Email">
           <Input />
         </Form.Item>
-        <Form.Item name='school_name' label='Name of School'>
+        <Form.Item name="school_name" label="Name of School">
           <Input />
         </Form.Item>
       </Form>
-      <Select
-        style={{ width: 70 }}
-        labelInValue
-        defaultValue={{ school_grade: 'lucy' }}
-        onChange={value => formHelper(value)}
-        name='school_grade_id'
-        label='Grade Level'
-      >
-        <Option value='None'>None</Option>
-        <Option value='KG 1'>KG 1</Option>
-        <Option value='KG 2'>KG 2</Option>
-        <Option value='KG 3'>KG 3</Option>
-        <Option value='Pri 1'>Pri 1</Option>
-        <Option value='Pri 2'>Pri 2</Option>
-        <Option value='Pri 3'>Pri 3</Option>
-        <Option value='Pri 4'>Pri 4</Option>
-        <Option value='Pri 5'>Pri 5</Option>
-        <Option value='Pri 6'>Pri 6</Option>
-        <Option value='Sec 1'>Sec 1</Option>
-        <Option value='Sec 2'>Sec 2</Option>
-        <Option value='Sec 3'>Sec 3</Option>
-        <Option value='Sec 4'>Sec 4</Option>
-        <Option value='Sec 5'>Sec 5</Option>
-        <Option value='Sec 6'>Sec 6</Option>
-      </Select>
+      <Form.Item name="school_grade_id" label="Grade Level">
+        <Select
+          style={{ width: 100 }}
+          defaultValue="Pri 6"
+          onChange={value => formHelper(value)}
+          name="school_grade_id"
+          label="Grade Level"
+        >
+          <Option value="None">None</Option>
+          <Option value="KG 1">KG 1</Option>
+          <Option value="KG 2">KG 2</Option>
+          <Option value="KG 3">KG 3</Option>
+          <Option value="Pri 1">Pri 1</Option>
+          <Option value="Pri 2">Pri 2</Option>
+          <Option value="Pri 3">Pri 3</Option>
+          <Option value="Pri 4">Pri 4</Option>
+          <Option value="Pri 5">Pri 5</Option>
+          <Option value="Pri 6">Pri 6</Option>
+          <Option value="Sec 1">Sec 1</Option>
+          <Option value="Sec 2">Sec 2</Option>
+          <Option value="Sec 3">Sec 3</Option>
+          <Option value="Sec 4">Sec 4</Option>
+          <Option value="Sec 5">Sec 5</Option>
+          <Option value="Sec 6">Sec 6</Option>
+        </Select>
+      </Form.Item>
     </>
   );
 };
