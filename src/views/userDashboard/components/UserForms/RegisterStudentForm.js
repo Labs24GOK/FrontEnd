00177f8b6620497484ee6b15608a2214518a7721
Alec Dye <alecdye@gmail.com>
2020-05-22 15:19:42 -0700

    Adding submit form modal notification and review page styling

--- src/views/userDashboard/components/UserForms/RegisterStudentForm.js
+++ src/views/userDashboard/components/UserForms/RegisterStudentForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Steps, Button, Row, Col, Layout } from 'antd';
+import { Steps, Button, Row, Col, Layout, Modal } from 'antd';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import { createNewStudent } from '../../../../actions/adminDashboardActions/studentTableActions';
+import { useHistory } from 'react-router-dom';
 
 // Sub component imports
 import StudentDetails from './StudentDetails';
@@ -18,6 +18,7 @@ const RegisterStudentForm = props => {
   const { Step } = Steps;
   const { Content } = Layout;
   const [studentForm, setStudentForm] = useState({ user_id: userID });
+  const history = useHistory();
 
   const handleChange = e => {
     setStudentForm({
@@ -27,27 +28,52 @@ const RegisterStudentForm = props => {
     console.log(studentForm);
   };
 
-  const formHelper = ({value}) => {
-    if(value === "M" || value === "F") {
+  const formHelper = ({ value }) => {
+    if (value === 'M' || value === 'F') {
       setStudentForm({
         ...studentForm,
-        gender: value
+        gender: value,
       });
-    } else if(value >= 1 && value <= 16) {
+    } else if (value >= 1 && value <= 16) {
       setStudentForm({
         ...studentForm,
-        school_grade_id: value
+        school_grade_id: value,
       });
     } else {
       setStudentForm({
         ...studentForm,
-        birthdate: value
+        birthdate: value,
       });
     }
   };
 
   const submitForm = values => {
     props.createNewStudent(studentForm);
+  };
+
+  // Success message modal
+  const countDown = () => {
+    let secondsToGo = 5;
+    const modal = Modal.success({
+      title: 'Student Registered!',
+      content: `Returning to Dashboard in ${secondsToGo} seconds.`,
+    });
+    const timer = setInterval(() => {
+      secondsToGo -= 1;
+      modal.update({
+        content: `Returning to Dashboard in ${secondsToGo} seconds.`,
+      });
+    }, 1000);
+    setTimeout(() => {
+      clearInterval(timer);
+      modal.destroy();
+    }, secondsToGo * 1000);
+  };
+
+  const buttonFunctions = () => {
+    submitForm();
+    countDown();
+    history.push('/dashboard');
   };
 
   const steps = [
@@ -78,13 +104,25 @@ const RegisterStudentForm = props => {
   function getStep(regState) {
     switch (regState) {
       case 0:
-        return <StudentDetails handleChange={handleChange} formHelper={formHelper} next={next}/>;
+        return (
+          <StudentDetails
+            handleChange={handleChange}
+            formHelper={formHelper}
+            next={next}
+          />
+        );
       case 1:
-        return <StudentAddress handleChange={handleChange} formHelper={formHelper} next={next}/>;
+        return (
+          <StudentAddress
+            handleChange={handleChange}
+            formHelper={formHelper}
+            next={next}
+          />
+        );
       case 2:
-        return <StudentContacts handleChange={handleChange} next={next}/>;
+        return <StudentContacts handleChange={handleChange} next={next} />;
       case 3:
-        return <StudentReview studentForm={studentForm} next={next}/>;
+        return <StudentReview studentForm={studentForm} next={next} />;
       default:
         return null;
     }
@@ -117,7 +155,7 @@ const RegisterStudentForm = props => {
               </Button>
             )}
             {regState === 3 ? (
-              <Button type="primary" onClick={submitForm}>
+              <Button type="primary" onClick={buttonFunctions}>
                 Submit
               </Button>
             ) : null}
--- src/views/userDashboard/components/UserForms/StudentReview.js
+++ src/views/userDashboard/components/UserForms/StudentReview.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Typography, Layout } from 'antd';
+import { Typography, Layout, Col, Row } from 'antd';
 
-const StudentReview = (props) => {
+const StudentReview = props => {
   const {
     user_id,
     cpr,
@@ -17,42 +17,140 @@ const StudentReview = (props) => {
     primary_emergency_contact_name,
     primary_emergency_phone,
     primary_emergency_relationship,
-    notes } = props.studentForm;
-  const { Title } = Typography;
+    notes,
+  } = props.studentForm;
+  const { Title, Text } = Typography;
   const { Content } = Layout;
 
   return (
     <Content style={{ margin: '1.8rem 0' }}>
       <Title level={3}>Review Registration</Title>
-    {console.log(props)}
-      <h4>cpr</h4>
-        <p>{cpr}</p>
-      <h4>First Name</h4>
-        <p>{first_name}</p>
-      <h4>Additional Names</h4>
-        <p>{additional_names}</p>
-      <h4>Birthday</h4>
-        <p>{birthdate}</p>
-      <h4>Gender</h4>
-        <p>{gender}</p>
-      <h4>School Name</h4>
-        <p>{school_name}</p>
-      <h4>School Grade</h4>
-        <p>{school_grade_id}</p>
-      <h4>Email</h4>
-        <p>{email}</p>
-      <h4>Address</h4>
-        <p>{address}</p>
-      <h4>Phone Number</h4>
-        <p>{phone_number}</p>
-      <h4>Primary Emergency Contact</h4>
-        <p>{primary_emergency_contact_name}</p>
-      <h4>Primary Emergency Relationship</h4>
-        <p>{primary_emergency_relationship}</p>
-      <h4>Primary Emergency Phone Number</h4>
-        <p>{primary_emergency_phone}</p>
-      <h4>Notes</h4>
-        <p>{notes}</p>
+      {console.log(props)}
+      <Row style={{ margin: '1.6rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">cpr</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{cpr}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">First Name</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{first_name}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">Additional Names</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{additional_names}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">Birthday</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{birthdate}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">Gender</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{gender}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">School Name</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{school_name}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">School Grade</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{school_grade_id}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">Email</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{email}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">Address</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{address}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">Phone Number</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{phone_number}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col>
+          <Text type="secondary">Primary Emergency Contact</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{primary_emergency_contact_name}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col>
+          <Text type="secondary">Primary Emergency Relationship</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{primary_emergency_relationship}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col>
+          <Text type="secondary">Primary Emergency Phone Number</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{primary_emergency_phone}</Text>
+        </Col>
+      </Row>
+
+      <Row style={{ margin: '1.8rem 0' }}>
+        <Col span={12}>
+          <Text type="secondary">Notes</Text>
+        </Col>
+        <Col span={12}>
+          <Text>{notes}</Text>
+        </Col>
+      </Row>
     </Content>
   );
 };
