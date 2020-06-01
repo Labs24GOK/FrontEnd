16e69443b0c127093b18e3803f78ac35f296fd3d
Alec Dye <alecdye@gmail.com>
2020-06-01 10:36:47 -0700

    Add password confirmation

--- src/authentication/Register.js
+++ src/authentication/Register.js
@@ -1,65 +1,92 @@
-import React from "react";
-import { withRouter } from "react-router";
-import { connect } from "react-redux";
-import { logIn } from "../actions/authenticationActions.js";
+import React, { useRef } from 'react';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+import { logIn } from '../actions/authenticationActions.js';
 import { familyRegister } from '../actions/registrationActions';
 import { useForm } from 'react-hook-form';
-import "./loginAndRegister.scss";
+import './loginAndRegister.scss';
 
 function Register(props) {
-
-	const { register, errors, handleSubmit } = useForm();
+  const { register, errors, handleSubmit, watch } = useForm();
+  const password = useRef({});
+  password.current = watch('password', '');
   const formSubmit = async user => {
+    // only parents will register through this form (no admin or staff)
+    // can change "parent" to another category later
+    user.user_type = 'parent';
 
-      // only parents will register through this form (no admin or staff)
-      // can change "parent" to another category later
-      user.user_type = "parent";
-      
-      // use old familyRegister action creator to register (for now)
-      await props.familyRegister(user, props.history);
+    // use old familyRegister action creator to register (for now)
+    await props.familyRegister(user, props.history);
   };
 
-    return (
-      
-          <div className="form-container">
-            <h1>Sign Up with The Garden of Knowledge</h1>
-            <form onSubmit={handleSubmit(formSubmit)}>
-              
-              <fieldset>
-                <label htmlFor="name">Full Name</label>
-                <input type="text" name="name" ref={register({required: true, minLength: 1})}/>
-                
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" ref={register({required: true, minLength: 1})}/>
+  return (
+    <div className="form-container">
+      <h1>Sign Up with The Garden of Knowledge</h1>
+      <form onSubmit={handleSubmit(formSubmit)}>
+        <fieldset>
+          <label htmlFor="name">Full Name</label>
+          <input
+            type="text"
+            name="name"
+            ref={register({ required: true, minLength: 1 })}
+          />
 
-                {/* <label htmlFor="username">Username</label>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            ref={register({ required: true, minLength: 1 })}
+          />
+
+          {/* <label htmlFor="username">Username</label>
                 <input type="text" name="username" ref={register({required: true, minLength: 1})}/> */}
 
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" ref={register({required: true, minLength: 1})}/>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            ref={register({ required: true, minLength: 1 })}
+          />
 
-                {/* add confirm password input w/ logic */}
+          {/* add confirm password input w/ logic */}
+          <label>Confirm Password</label>
+          <input
+            name="confirm_pass"
+            type="password"
+            ref={register({
+              validate: value =>
+                value === password.current || 'Passwords do not match',
+            })}
+          />
+          {errors.confirm_pass && <p>{errors.confirm_pass.message}</p>}
 
-                <button type="submit" className={errors.name ? "disabled" : ""}>
-                  Register
-                </button>
-              </fieldset>
-              
-              <div className="register-errors">
-                {(errors.name || errors.email || errors.password) && <p>All fields are required.</p>}
+          <button type="submit" className={errors.name ? 'disabled' : ''}>
+            Register
+          </button>
+        </fieldset>
 
-              {/* if the backend returned an error because the username or email already exists, display that error */}
-              {props.state && props.state.registrationReducer && props.state.registrationReducer.familyRegister && props.state.registrationReducer.familyRegister.error && <p>{props.state.registrationReducer.familyRegister.error}</p>}
+        <div className="register-errors">
+          {(errors.name || errors.email || errors.password) && (
+            <p>All fields are required.</p>
+          )}
 
-              </div>
-            </form>
-          </div>
-
-    );
-  }
+          {/* if the backend returned an error because the username or email already exists, display that error */}
+          {props.state &&
+            props.state.registrationReducer &&
+            props.state.registrationReducer.familyRegister &&
+            props.state.registrationReducer.familyRegister.error && (
+              <p>{props.state.registrationReducer.familyRegister.error}</p>
+            )}
+        </div>
+      </form>
+    </div>
+  );
+}
 
 const mapStateToProps = state => {
   return { state: state };
 };
 
-export default withRouter(connect( mapStateToProps, { logIn, familyRegister } )(Register));
+export default withRouter(
+  connect(mapStateToProps, { logIn, familyRegister })(Register)
+);
