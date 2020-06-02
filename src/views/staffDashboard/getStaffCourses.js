d06bf9fd73c35dac8fbab36162be9e4b83d30f8c
Gremlin4544 <alve@mckethen.com>
2020-06-02 18:46:20 -0400

    delete unneeded components

--- src/views/staffDashboard/getStaffCourses.js
+++
@@ -1,8 +0,0 @@
-import axiosWithAuth from "../../utils/axiosWithAuth";
-
-export const getStaffCourses = async (staffID) => {
-
-    let result = await axiosWithAuth()
-        .get(`/staff/${staffID}/courses`)
-    return result.data;
-}
