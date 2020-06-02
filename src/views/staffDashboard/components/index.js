04a6ffd79090f2c643daf90603247d304a4ed750
Gremlin4544 <alve@mckethen.com>
2020-06-02 15:24:24 -0400

    display routes on index for header & dashboard

--- src/views/staffDashboard/components/index.js
+++ src/views/staffDashboard/components/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+
+import StaffDashboardHeader from './StaffDashboardHeader';
+import StaffDashboard from './StaffDashboard';
+import Footer from '../../marketing/components/Footer';
+import '../../marketing/marketing.scss';
 
 function Index() {
-  return (<h1>The staff dashboard will be redesigned soon.</h1>)
+  let { path, url } = useRouteMatch();
+
+  return (
+  <div className="content">
+    
+    <StaffDashboardHeader />
+    <Switch>  
+      <Route exact path={path}>
+        <StaffDashboard />  
+    </Route>
+    </Switch>
+    <Footer />
+  </div>
+  )
 }
 
 export default Index;
