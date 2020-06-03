dd57a07b186fa0fba3f0cc56afb2386d72fc062d
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-06-03 10:01:44 -0700

    fix course deletion crashing app after redirect

--- src/views/adminDashboard/components/courses/courseCardTabs/CourseInformationTab.js
+++ src/views/adminDashboard/components/courses/courseCardTabs/CourseInformationTab.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {
   getCourseById,
@@ -22,9 +22,11 @@ import { timeConverter, dateConverter } from '../../../../../utils/helpers.js';
 import Modal from '../../modals/DeleteModal';
 
 const CourseInformationTab = props => {
+  const { push } = useHistory()
+  const courseInfo = props.getCourseById
   useEffect(() => {
-    props.getCourseById(props.courseID);
-  }, []);
+    courseInfo()
+  }, [courseInfo]);
 
   const editCourseInfo = e => {
     e.preventDefault();
@@ -43,128 +45,130 @@ const CourseInformationTab = props => {
 
   const deleteCourseInfo = async () => {
     await props.deleteCourseById(props.courseById.course_id);
-    setTimeout(() => {
-      props.setCourseView('courseTableView');
-    }, 500);
+      push('/dashboard/courses')
   };
 
   return (
     <div>
-      {!props.isEditing ? (
-        <>
-          <FormWrap>
-            <FormSet>
-              <Div>
-                <div>
-                  <Label>Status</Label>
+      {courseInfo ? (
+        !props.isEditing ? (
+          <>
+            <FormWrap>
+              <FormSet>
+                <Div>
+                  <div>
+                    <Label>Status</Label>
 
-                  <TextDiv>
-                    {(props.courseById && props.courseById.status) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>Term</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.term) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>Course Type</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.course_type) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>Group Type</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.group_type) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>Level</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.level) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>Section</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.section) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>School Grade</Label>
-                  <TextDiv>{props.courseById.school_grade || '-'}</TextDiv>
-                </div>
-                <div>
-                  <Label>Course Schedule</Label>
-                  <TextDiv>{props.courseById.course_schedule || '-'}</TextDiv>
-                </div>
-                <div>
-                  <Label>Start Date</Label>
-                  <TextDiv>{startDate || '-'}</TextDiv>
-                </div>
-                <div>
-                  <Label>End Date</Label>
-                  <TextDiv>{endDate || '-'}</TextDiv>
-                </div>
-                <div>
-                  <Label>Start Time</Label>
-                  <TextDiv>{startTime || '-'}</TextDiv>
-                </div>
-                <div>
-                  <Label>End Time</Label>
-                  <TextDiv>{endTime || '-'}</TextDiv>
-                </div>
-                <div>
-                  <Label>Room</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.room) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>Teacher</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.teacher) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>Hourly Rate</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.hourly_rate) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>Status</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.status) || '-'}
-                  </TextDiv>
-                </div>
-                <div>
-                  <Label>Notes</Label>
-                  <TextDiv>
-                    {(props.courseById && props.courseById.notes) || '-'}
-                  </TextDiv>
-                </div>
-              </Div>
-              <Div>
-                <div style={{ gridColumn: 'span3' }}></div>
-              </Div>
-            </FormSet>
-            <ButtonDiv>
-              <SaveButton type="submit" onClick={editCourseInfo}>
-                Edit
-              </SaveButton>
-              <DeleteButton type="submit" onClick={areYouSureYouWantToDelete}>
-                Delete
-              </DeleteButton>
-            </ButtonDiv>
-          </FormWrap>
-          <Modal submitActionCB={deleteCourseInfo} />
-        </>
-      ) : (
-        <CourseEditForm {...props} />
-      )}
+                    <TextDiv>
+                      {(props.courseById && props.courseById.status) || '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>Term</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.term) || '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>Course Type</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.course_type) ||
+                        '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>Group Type</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.group_type) || '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>Level</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.level) || '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>Section</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.section) || '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>School Grade</Label>
+                    <TextDiv>{props.courseById.school_grade || '-'}</TextDiv>
+                  </div>
+                  <div>
+                    <Label>Course Schedule</Label>
+                    <TextDiv>{props.courseById.course_schedule || '-'}</TextDiv>
+                  </div>
+                  <div>
+                    <Label>Start Date</Label>
+                    <TextDiv>{startDate || '-'}</TextDiv>
+                  </div>
+                  <div>
+                    <Label>End Date</Label>
+                    <TextDiv>{endDate || '-'}</TextDiv>
+                  </div>
+                  <div>
+                    <Label>Start Time</Label>
+                    <TextDiv>{startTime || '-'}</TextDiv>
+                  </div>
+                  <div>
+                    <Label>End Time</Label>
+                    <TextDiv>{endTime || '-'}</TextDiv>
+                  </div>
+                  <div>
+                    <Label>Room</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.room) || '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>Teacher</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.teacher) || '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>Hourly Rate</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.hourly_rate) ||
+                        '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>Status</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.status) || '-'}
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <Label>Notes</Label>
+                    <TextDiv>
+                      {(props.courseById && props.courseById.notes) || '-'}
+                    </TextDiv>
+                  </div>
+                </Div>
+                <Div>
+                  <div style={{ gridColumn: 'span3' }}></div>
+                </Div>
+              </FormSet>
+              <ButtonDiv>
+                <SaveButton type='submit' onClick={editCourseInfo}>
+                  Edit
+                </SaveButton>
+                <DeleteButton type='submit' onClick={areYouSureYouWantToDelete}>
+                  Delete
+                </DeleteButton>
+              </ButtonDiv>
+            </FormWrap>
+            <Modal submitActionCB={deleteCourseInfo} />
+          </>
+        ) : (
+          <CourseEditForm {...props} />
+        )
+      ) : null}
     </div>
   );
 };
