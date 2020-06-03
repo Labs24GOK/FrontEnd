97ef115c78a4e138529c1dfdedc152171e9bc0dc
Alec Dye <alecdye@gmail.com>
2020-06-03 10:51:18 -0700

    Add handleSubmit

--- src/views/userDashboard/components/UserSettingsEdit.js
+++ src/views/userDashboard/components/UserSettingsEdit.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useRouteMatch, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Input, Typography, Layout, Col, Row, Button, Form } from 'antd';
 
 const UserSettings = () => {
   const { Title, Text } = Typography;
   const { Content } = Layout;
   const [userState, setUserState] = useState([]);
-  const { url } = useRouteMatch();
   const history = useHistory();
 
   const handleCancel = e => {
@@ -14,42 +13,52 @@ const UserSettings = () => {
     history.push(`/dashboard/account-settings`);
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  // useEffect to get user data and save to state and populate forms
+
+  // onSubmit to PUT new data to backend
+
   return (
     <Content style={{ margin: '1.8rem 0' }}>
-      <Row>
-        <Col>
-          <Title level={3}>Account Edit</Title>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Item name="name" label="Full Name">
-            <Input />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Item name="email" label="Email">
-            <Input />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Item name="password" label="Password">
-            <Input />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Button onClick={handleCancel}>Cancel</Button>
-        </Col>
-        <Col>
-          <Button>Save</Button>
-        </Col>
-      </Row>
+      <Form>
+        <Row>
+          <Col>
+            <Title level={3}>Account Edit</Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Item name="name" label="Full Name">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Item name="email" label="Email">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Item name="password" label="Password">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button onClick={handleCancel}>Cancel</Button>
+          </Col>
+          <Col>
+            <Button type="primary">Save</Button>
+          </Col>
+        </Row>
+      </Form>
     </Content>
   );
 };
