d00259a48eb27ce63e78383d5e215ee040d0e638
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-06-03 19:08:35 -0700

    create logic & display staffCourseCard.

--- src/views/staffDashboard/components/StaffCourseCard.js
+++ src/views/staffDashboard/components/StaffCourseCard.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axiosWithAuth from '../../../utils/axiosWithAuth';
 
 import './staffDashboard.scss';
 
 const StaffCourseCard = () => {
-    let params = useParams();
-    console.log(params);
-    return (
-        <div className="staff-courses">
+	let {courseId} = useParams();
+	const [courseData, setCourseData] = useState({})
 
-                <h1>Pickles</h1>
+	useEffect(() => {
+		axiosWithAuth()
+			.get(`/course/${courseId}`)
+			.then(res => {
+                console.log(res.data);
+                setCourseData(res.data)
+			})
+			.catch(err => console.log(err));
+	}, []);
 
-        </div>
-    )
+	return (
+		<div className='staff-courses'>
+            <h2>{courseData.teacher}</h2>
+			<h4>Course ID: {courseData.course_id}</h4>
+            <h4>{courseData.group_type}  --  {courseData.level}</h4>
+            <h4>{courseData.term} -- {courseData.course_schedule} </h4>
+    <h5>{courseData.start_time} - {courseData.end_time}</h5>
+            <h4> Course Type: {courseData.course_type}</h4>
+            <h4> Hourly Rate: {courseData.hourly_rate}</h4>
+            <h4> Room: {courseData.room_id} -- Chairs: {courseData.room}</h4>
+            <h4> Notes: {courseData.notes}</h4>
+            
+		</div>
+	);
 };
 
 export default StaffCourseCard;
-
