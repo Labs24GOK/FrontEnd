1dd8be9771b540ded8871e5d02f950eb006947ac
c0derbr1t <c0der.br1t@gmail.com>
2020-06-04 17:30:24 -0500

    Add more course information and clean up StudentDetails.js

--- src/views/userDashboard/components/Student/StudentDetails.js
+++ src/views/userDashboard/components/Student/StudentDetails.js
@@ -1,30 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { EditButton } from '../../../adminDashboard/components/mainStyle/styledComponent';
 import { getDateStringENGBFormat } from '../../../../utils/helpers';
 
 function StudentDetails(props) {
 
-console.log("SD-course: ", props.course);
-
     return (
-        <>    
-            <div className="studentBox" >  
+        <>  
+            <h1 className='studentTitle'>
+                {props.student.first_name}
+                &nbsp;  
+                {props.student.additional_names}
+                <h4>Government ID: {props.student.cpr}</h4>
+            </h1> 
+            <div className="studentBox">
                 <div className="studentInfoBox">
                     <h2>Details</h2>
                     <hr></hr>
                     <div className="studentSubBox">
                         <div>
                             <h3>Student General Info:</h3>
-                            <p><strong>Birthdate:</strong> {getDateStringENGBFormat(props.student.birthdate)}</p>
-                            <p><strong>Gender:</strong> {props.student.gender}</p>
-                            <p><strong>School Name:</strong> {props.student.school_name}</p>
+                            <p>
+                                <strong>Birthdate:</strong>
+                                <br></br>
+                                {getDateStringENGBFormat(props.student.birthdate)}
+                            </p>
+                            <p>
+                                <strong>Gender:</strong> 
+                                <br></br>
+                                {props.student.gender}
+                            </p>
+                            <p>
+                                <strong>School Name:</strong> 
+                                <br></br>
+                                {props.student.school_name}
+                            </p>
                         </div>
                         <div>
                             <h3>Student Contact Info:</h3>
-                            <p><strong>Email:</strong> {props.student.email}</p>
-                            <p><strong>Phone Number:</strong> {props.student.phone_number}</p>
-                            <p><strong>Address:</strong> {props.student.address}</p>
+                            <p>
+                                <strong>Email:</strong> 
+                                <br></br>
+                                {props.student.email}
+                            </p>
+                            <p>
+                                <strong>Phone Number:</strong> 
+                                <br></br>
+                                {props.student.phone_number}
+                            </p>
+                            <p>
+                                <strong>Address:</strong> 
+                                <br></br>
+                                {props.student.address}
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -34,15 +62,39 @@ console.log("SD-course: ", props.course);
                     <div className="studentSubBox">
                         <div>
                             <h3>Primary Contact:</h3>
-                            <p><strong>Name:</strong> {props.student.primary_emergency_contact_name}</p>
-                            <p><strong>Relationship:</strong> {props.student.primary_emergency_relationship}</p>
-                            <p><strong>Phone Number:</strong> {props.student.primary_emergency_phone}</p>
+                            <p>
+                                <strong>Name:</strong> 
+                                <br></br>
+                                {props.student.primary_emergency_contact_name}
+                            </p>
+                            <p>
+                                <strong>Relationship:</strong> 
+                                <br></br>
+                                {props.student.primary_emergency_relationship}
+                            </p>
+                            <p>
+                                <strong>Phone Number:</strong> 
+                                <br></br>
+                                {props.student.primary_emergency_phone}
+                            </p>
                         </div>
                         <div>
                             <h3>Secondary Contact:</h3>
-                            <p><strong>Name:</strong> {props.student.emergency_contact_name ? props.student.emergency_contact_name : "N/A"}</p>
-                            <p><strong>Relationship:</strong> {props.student.emergency_relationship ? props.student.emergency_relationship : "N/A"}</p>
-                            <p><strong>Phone Number:</strong> {props.student.emergency_phone ? props.student.emergency_phone : "N/A"}</p>
+                            <p>
+                                <strong>Name:</strong> 
+                                <br></br>
+                                {props.student.emergency_contact_name ? props.student.emergency_contact_name : "N/A"}
+                            </p>
+                            <p>
+                                <strong>Relationship:</strong> 
+                                <br></br>
+                                {props.student.emergency_relationship ? props.student.emergency_relationship : "N/A"}
+                            </p>
+                            <p>
+                                <strong>Phone Number:</strong> 
+                                <br></br>
+                                {props.student.emergency_phone ? props.student.emergency_phone : "N/A"}
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -57,7 +109,9 @@ console.log("SD-course: ", props.course);
                                 <p>No available notes for this student.</p>
                             </div>
                             :
-                            <p><strong>Notes: </strong>{props.student.notes}</p>
+                            <div>
+                                <p><strong>Notes: </strong>{props.student.notes}</p>
+                            </div>
                         }
                     </div>
                 </div>
@@ -76,64 +130,69 @@ console.log("SD-course: ", props.course);
                     props.course.map((course) => (
                         <>
                             <div className="studentSubBox">
-                                <div>
-                                    <p>
-                                        <strong>Term: </strong>
-                                        {course.term}
-                                        &emsp;
-                                        &emsp;
-                                        <strong>First Day: </strong>
-                                        {getDateStringENGBFormat(course.first_day)} 
-                                        &emsp; 
-                                        &emsp;
-                                        <strong>Last Day: </strong>
-                                        {getDateStringENGBFormat(course.last_day)}
-                                        &emsp;
-                                        &emsp;
-                                        <strong>Course Days: </strong>
-                                        {course.course_days}
-                                        &emsp;
-                                        &emsp;
-                                        <strong>Start Time: </strong>
-                                        {course.start_time}
-                                        &emsp;
-                                        &emsp;
-                                        <strong>End Time: </strong>
-                                        {course.end_time}
-                                    </p>
-                                    <p>
-                                        <strong>Course Type: </strong>
-                                        {course.course_type}
-                                        &emsp;
-                                        &emsp;
-                                        <strong>Group Type: </strong>
-                                        {course.group_type}
-                                        &emsp;
-                                        &emsp;
-                                        <strong>Level: </strong>
-                                        {course.course_level}
-                                        &emsp;
-                                        &emsp;
-                                        <strong>Section: </strong>
-                                        {course.section}
-                                        &emsp;
-                                        &emsp;
-                                        <strong>Course Status: </strong>
-                                        {course.course_status}
-                                        &emsp;
-                                        &emsp;
-                                        <strong>Student Status: </strong>
-                                        {course.student_result_type}
-                                    </p>
-                                    <p>
-                                        <strong>Notes: </strong>
-                                        {course.notes}
-                                    </p>
+                                <div className="studentCourseBox">
+                                    <div>
+                                        <p><strong>Term: </strong>
+                                        <br></br>
+                                        {course.term}</p>
+                                        <p><strong>First Day: </strong>
+                                        <br></br>
+                                        {getDateStringENGBFormat(course.first_day)}</p>
+                                        <p><strong>Last Day: </strong>
+                                        <br></br>
+                                        {getDateStringENGBFormat(course.last_day)}</p>
+                                    </div>
+                                    <div>
+                                        <p><strong>Course Days: </strong>
+                                        <br></br>
+                                        {course.course_days}</p>
+                                        <p><strong>Start Time: </strong>
+                                        <br></br>
+                                        {course.start_time}</p>
+                                        <p><strong>End Time: </strong>
+                                        <br></br>
+                                        {course.end_time}</p>
+                                    </div>
+                                    <div>
+                                        <p><strong>Course Type: </strong>
+                                        <br></br>
+                                        {course.course_type}</p>
+                                        <p><strong>Course Status: </strong>
+                                        <br></br>
+                                        {course.course_status}</p>
+                                        <p><strong>Student Status: </strong>
+                                        <br></br>
+                                        {course.student_result_type}</p>
+                                    </div>
+                                    <div>
+                                        <p><strong>Group Type: </strong>
+                                        <br></br>
+                                        {course.group_type}</p>
+                                        <p><strong>Level: </strong>
+                                        <br></br>
+                                        {course.course_level}</p>
+                                        <p><strong>Section: </strong>
+                                        <br></br>
+                                        {course.section}</p>
+                                    </div>
+                                    {course.notes === "" ? 
+                                        <div className="noInfo">
+                                            <p><strong>Notes: </strong>
+                                            <br></br>
+                                            No available notes for this course.</p>
+                                        </div>
+                                    :
+                                        <div>
+                                            <p><strong>Notes: </strong>
+                                            <br></br>
+                                            {course.notes}</p>
+                                        </div>
+                                    }
                                 </div>
                             </div>
-                            <hr></hr> 
-                        </> 
-                    ))}
+                            <hr></hr>
+                        </>
+                    ))}       
                     {props.course.length === 0 && 
                         <div className="studentSubBox">
                             <div className="noInfo">
@@ -147,4 +206,13 @@ console.log("SD-course: ", props.course);
     )
 };
 
-export default StudentDetails;                
+export default StudentDetails;
+
+// const mapStateToProps = state => {
+//     return {
+//         studentById: state.studentByIdReducer.studentById,
+//         error: state.studentByIdReducer.error
+//     };
+// };
+
+// export default connect(mapStateToProps, {})(StudentDetails);                
