33b1da526acec7990f10788e1ee858a5cd89f71f
Alec Dye <alecdye@gmail.com>
2020-06-04 09:31:15 -0700

    Build fetch requests

--- src/views/userDashboard/components/UserSettingsEdit.js
+++ src/views/userDashboard/components/UserSettingsEdit.js
@@ -1,29 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import axiosWithAuth from '../../../utils/axiosWithAuth';
 import { Input, Typography, Layout, Col, Row, Button, Form } from 'antd';
 
 const UserSettings = () => {
   const { Title, Text } = Typography;
   const { Content } = Layout;
-  const [userState, setUserState] = useState([]);
   const history = useHistory();
+  const [userData, setUserData] = useState({
+    userID: null,
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  // userID from JWT
+  const token = localStorage.getItem('token');
+  const tokenData = JSON.parse(atob(token.split('.')[1]));
+  const userID = tokenData.subject;
 
   const handleCancel = e => {
     e.preventDefault();
     history.push(`/dashboard/account-settings`);
   };
 
+  // submitting updated user data
+  //! no endpoints for updating user?
   const handleSubmit = e => {
     e.preventDefault();
+    axiosWithAuth()
+      .put('')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
-  // useEffect to get user data and save to state and populate forms
+  // fetching user's data for form inputs
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/users/${userID}`)
+      .then(res => {
+        console.log(res.data[0]);
+        setUserData({ ...userData });
+      })
+      .catch(err => {
+        console.log('GET failed', err);
+      });
+  }, []);
 
   // onSubmit to PUT new data to backend
 
   return (
     <Content style={{ margin: '1.8rem 0' }}>
-      <Form>
+      <Form handleSubmit={handleSubmit}>
         <Row>
           <Col>
             <Title level={3}>Account Edit</Title>
