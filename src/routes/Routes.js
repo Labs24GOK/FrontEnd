2297e6163885ead05845fde65dccba6a947d9a28
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-06-08 17:49:37 -0700

    Remove nested Header & Footer from nesting in routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -19,8 +19,8 @@ function Routes() {
         <Route path="/schedules" render={() => <Marketing page="course_structure" />}/>
         <Route path="/courses" render={() => <Marketing page="courses" />}/>
         <Route path="/about" render={() => <Marketing page="about" />}/>
-        <Route path="/login" render={() => <><Header /><Login /><Footer /></>}/>
-        <Route path="/register" render={() => <><Header /><Register /><Footer /></>}/>
+        <Route path="/login" render={() => <Login />}/>
+        <Route path="/register" render={() => <Register />}/>
       </Switch>
     </>
   );
