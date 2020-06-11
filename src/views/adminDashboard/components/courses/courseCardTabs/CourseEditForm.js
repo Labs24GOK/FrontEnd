743018853b7658a4276bc82fbf87c11cec36012f
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-06-11 08:54:18 -0700

    refactored edit form to reflect new input style

--- src/views/adminDashboard/components/courses/courseCardTabs/CourseEditForm.js
+++ src/views/adminDashboard/components/courses/courseCardTabs/CourseEditForm.js
@@ -9,8 +9,24 @@ import { ButtonDiv,  CancelButton,  Div,  FormSet,  FormWrap,  Input,  Label,  S
 
 const CourseEditForm = props => {
   const { courseID } = props;
-  const { register, errors, handleSubmit, watch } = useForm();
-  const dropDowns = ['term_id', 'course_type_id', 'group_type_id', 'school_grade_id', 'level_id', 'course_schedule_id',  'room_id', 'teacher_id']
+  const { 
+    course_schedule,
+    course_type,
+    end_time,
+    group_type,
+    hourly_rate,
+    level,
+    notes,
+    room,
+    school_grade,
+    section,
+    start_time,
+    status,
+    teacher_id,
+    term
+   } = props.courseById
+  const { register, errors, handleSubmit } = useForm();
+  const dropDowns = ['teacher_id']
   
   const submitNow = data => {
     for (const property of dropDowns) {
@@ -18,8 +34,6 @@ const CourseEditForm = props => {
     }
     props.editCourseById(courseID, data);
   }
-
-  const currentlySelectedCourse = watch("course_type_id");
 
   let startdate = new Date(props.courseById.start_date)
     .toISOString()
@@ -37,138 +51,147 @@ const CourseEditForm = props => {
 
 return (
   <FormWrap onSubmit={handleSubmit(submitNow)}>
-    <FormSet>
-      <Div>
-        <div>
-          <Label>Status</Label>
-            <div>
-              <select name="status" defaultValue={props.courseById.status} className="dropDown" ref={register({required: true})}>
-                <option value="Active">Active</option>
-                <option value="Completed">Completed</option>
-                <option value="Waitlist">Waitlist</option>
-                <option value="Cancelled">Cancelled</option>
-              </select>
-            </div>
-        </div>
-        <div>
-          <Label>Term</Label>
-            <select name="term_id" defaultValue={props.courseById.term_id} className="dropDown" ref={register({required: true})}>
-              {createDropdown(props.termDropdown)}
-            </select>
-        </div>
-        <div>
-          <Label>Course Type</Label>
-            <select name="course_type_id" defaultValue ={props.courseById.course_type_id} className="dropDown" ref={register({required: true})}>
-              {createDropdown(props.courseTypeDropdown)}
-            </select>   
-        </div>
-        <div>
+      <FormSet>
+        <Div>
+          <div>
+            <Label>Term</Label>
+              <select className="dropDown" name="term" defaultValue={term} ref={register({required: true })}>
+                <option value="Fall">Fall</option>
+                <option value="Winter">Winter</option>
+                <option value="Spring">Spring</option>
+                <option value="Summer">Summer</option>
+              </select>	 
+          </div> 
+
+          <div>
+            <Label>Course Type</Label>
+              <div>
+                <Input type="text" className={course_type && "input-error"} name="course_type" defaultValue={course_type} ref={register({required: true})}/>
+                  {errors.course_type && errors.course_type.type === "required" && 'Course type is Required'}
+              </div>
+          </div>
+          
+          <div>
               <Label>Group Type</Label>
 						<div>
-              <select name="group_type_id" defaultValue={props.courseById.group_type_id} className="dropDown" ref={register({required: true})}>
-                {createDropdown(props.groupTypeDropdown)}
-              </select>
+              <Input name="group_type" placeholder='Goverment' className="dropDown" defaultValue={group_type} ref={register({required: true})} />
+              {errors.group_type && errors.group_type.type === "required" && 'Group type is Required'}
 					  </div>
-        </div>  
-        <div>
-              <Label>Level</Label>
+          </div>
+
+          <div>
+            <Label>School Grade</Label>
+            <div>
+              <Input name="school_grade" placeholder='KG 1' defaultValue={school_grade} ref={register({required: false})} />
+					  </div>
+          </div>
+
+            <div>
+              <Label>Course Level</Label>
               <div>
-                <select name="level_id" defaultValue= {props.courseById.level_id} className="dropDown" ref={register({required: true})}>
-                  {createDropdown(props.levelDropdown)}
-                </select>  
+                <Input className="dropDown" name="level" defaultValue={level} ref={register({required: true})} />
 						  </div>
-        </div> 
-        <div>
+          </div>
+
+          <div>
             <Label>Section</Label>
               <div>
-                <select name="section"  defaultValue ={props.courseById.section} className="dropDown" ref={register({required: true})}>
-                  <option value="A">A</option>
-                  <option value="B">B</option>
-                  <option value="C">C</option>
-                </select>  
+                <Input name="section" defaultValue={section} ref={register({required: false})} />
               </div>
-        </div>   
-        <div>
-          <Label>School Grade</Label>
-          <select className={"dropDown " + (currentlySelectedCourse !== "2" ? "grey" : "")} name="school_grade_id" defaultValue={props.courseById.school_grade_id} disabled={currentlySelectedCourse !== "2"} ref={register({required: true})}>
-              {createDropdown(props.schoolGradeDropdown)}
-          </select>  
-        </div>
-        <div>
+          </div>
+
+          <div>
             <Label>Course Schedule</Label>
               <div>
-                <select name="course_schedule_id"  className="dropDown" defaultValue={props.courseById.course_schedule} ref={register({required: true})}>
-                  {createDropdown(props.courseScheduleDropdown)}
-                </select>
-              </div>
-          </div> 
-          <div>
-            <Label>Start Date</Label>
-              <div>
-                <Input name="start_date" type="date" defaultValue={startdate} className={errors.start_date && "input-error"} ref={register({required: true})}/>
-                  {errors.start_date && errors.start_date.type === "required" && 'Start Date is Required'}
+                <Input placeholder='Sat / Sun / Tue'  name="course_schedule" defaultValue={course_schedule} ref={register({required: true})} />
               </div>
           </div>
-          <div>
-            <Label>End Date</Label>
-              <div>
-                <Input name="end_date" type="date" defaultValue={enddate} className={errors.end_date && "input-error"} ref={register({required: true})}/>
-                  {errors.end_date && errors.end_date.type === "required" && 'End Date is Required'}
-              </div>
-          </div>     
-          <div>
-            <Label>Start Time</Label>
-              <div>
-                <Input name="start_time" type="time" defaultValue={props.courseById.start_time} className={errors.start_time && "input-error"} ref={register({required: true})} />
-                  {errors.start_time && errors.start_time.type === "required" && 'Start Time is Required'}	
-              </div>
-          </div>
-          <div>
-            <Label>End Time</Label>
-              <div>
-                <Input name="end_time" defaultValue={props.courseById.end_time} type="time" className={errors.end_time && "input-error"} ref={register({required: true})}/>
-                  {errors.end_time && errors.end_time.type === "required" && 'End Time is Required'}
-              </div>
-          </div>
+
           <div>
             <Label>Room</Label>
               <div>
-                <select name="room_id" defaultValue={`${props.courseById.room}`} className="dropDown" ref={register({required: true})}>
-                  {createDropdown(props.roomDropdown)}
-                </select>
+                <Input name="room" defaultValue={room} ref={register({required: true})} />
               </div>
           </div>
+
+          <div>
+            <Label>Start Date</Label>
+              <div>
+                <Input type="date" className={errors.start_date && "input-error"} name="start_date" defaultValue={startdate} ref={register({required: true})}/>
+                  {errors.start_date && errors.start_date.type === "required" && 'Start Date is Required'}
+              </div>
+          </div>
+
+          <div>
+            <Label>End Date</Label>
+              <div>
+                <Input type="date" className={errors.end_date && "input-error"} name="end_date" defaultValue={enddate} ref={register({required: true})}/>
+                  {errors.end_date && errors.end_date.type === "required" && 'End Date is Required'}
+              </div>
+          </div>
+
+          <div>
+            <Label>Start Time</Label>
+              <div>
+                <Input type="time" className={errors.start_time && "input-error"} name="start_time" defaultValue={start_time} ref={register({required: true})}/>
+                  {errors.start_time && errors.start_time.type === "required" && 'Start Time is Required'}	
+              </div>
+          </div>
+
+          <div>
+            <Label>End Time</Label>
+              <div>
+                <Input type="time" className={errors.end_time && "input-error"} name="end_time" defaultValue={end_time} ref={register({required: true})}/>
+                  {errors.end_time && errors.end_time.type === "required" && 'End Time is Required'}
+              </div>
+          </div>
+
           <div>
             <Label>Teacher</Label>
               <div>
-                <select name="teacher_id" defaultValue ={props.courseById.teacher} className="dropDown" ref={register({required: true})}>
+                <select className="dropDown"  name="teacher_id" defaultValue={teacher_id} ref={register({required: true})}>
                   {createDropdown(props.teacherDropdown)}
                 </select>
               </div>
           </div>
+
           <div>
             <Label>Hourly Rate</Label>
               <div>
-                <Input name="hourly_rate" defaultValue = {props.courseById.hourly_rate} type="text" className={errors.hourly_rate && "input-error"} ref={register({required: true})} />
+                <Input type="text" placeholder='14' className={errors.hourly_rate && "input-error"} name="hourly_rate" defaultValue={hourly_rate} ref={register({required: true})}/>
                   {errors.hourly_rate && errors.hourly_rate.type === "required" && 'Hourly Rate is Required'}
               </div>
           </div>
+
           <div>
-            <Label>Notes</Label>
+            <Label>Status</Label>
               <div>
-                <Input name="notes" defaultValue = {props.courseById.notes} type="text" ref={register} />
+                <select className="dropDown"  name="status" defaultValue={status} ref={register({required: true})}>
+                  <option value="Active">Active</option>
+                  <option value="Completed">Completed</option>
+                  <option value="Waitlist">Waitlist</option>
+                  <option value="Cancelled">Cancelled</option>
+                </select>
               </div>
-          </div>   
-          </Div>
-    </FormSet>
+          </div>
+
+          <div>
+            <Label>Course Notes</Label>
+              <div>
+                <Input type="text" name="notes" defaultValue={notes} ref={register}/>
+              </div>
+          </div>    
+
+        </Div>
+      </FormSet>
         <ButtonDiv>
-        <CancelButton onClick={handleCancel}>Cancel</CancelButton>
-        <SaveButton type='submit' onClick={handleSubmit}>
-          Save
-        </SaveButton>
-      </ButtonDiv>
-  </FormWrap>
-);
+          <CancelButton onClick={handleCancel}>
+            Cancel
+          </CancelButton>
+          <SaveButton onClick={handleSubmit} type='submit'>Save</SaveButton>
+        </ButtonDiv>
+    </FormWrap>
+  );
 };
 
 const mapStateToProps = state => {
