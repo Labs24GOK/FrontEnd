b150f334cb6435902329a18210af71a6e81a4541
Alec Dye <alecdye@gmail.com>
2020-06-12 14:19:16 -0700

    Refactor styles

--- src/views/userDashboard/components/Student/StudentCourseCard.js
+++ src/views/userDashboard/components/Student/StudentCourseCard.js
@@ -1,53 +1,56 @@
 import React, { useEffect, useState } from 'react';
-
 import { getStudentCourses } from '../../getStudentCourses';
 import { getDateStringENGBFormat, timeConverter } from '../../../../utils/helpers';
+import './StudentCourseCard.scss';
 
 // Sets Course state for students
 function StudentCourseCard({ student }) {
-  const [studentCourse, setStudentCourse] = useState([]);  
-  const [courseBool, setCourseBool] = useState(false);  
+  const [studentCourse, setStudentCourse] = useState([]);
+  const [courseBool, setCourseBool] = useState(false);
 
   useEffect(() => {
-    if(!courseBool) {
+    if (!courseBool) {
       getStudentCourses(student.id)
         .then(res => {
-          setStudentCourse(res)
-          setCourseBool(true)
+          setStudentCourse(res);
+          setCourseBool(true);
         })
         .catch(err => console.log(err));
     }
   }, [studentCourse]);
 
   return (
-    <div className="studentCourseCard">
-      <div className="nameAndHamburgerMenu">
-        <div className="names">
-     
-          <h2 className="firstName">{student.first_name}</h2>
-          <h3 className="additionalNames">{student.additional_names}</h3>
- 
-
-          <h4 className="courseAmount">{studentCourse.length} course(s)</h4>
-
-        </div>
+    <div className="container">
+      <div className="student-card">
+        <h2>
+          {student.first_name}
+          {student.additional_names}
+        </h2>
       </div>
-      {!studentCourse || studentCourse.length === 0 ? (
-        <p className="coursesList">{student.first_name} has not registered for any courses yet.</p>
-      ) : (
-        <div className="coursesList">
-          {studentCourse.map(course => (
-            <div key={course.course_id} className="studentSubBox">
-              <h3>{course.group_type}</h3>
-              <h3>{course.course_days}<br />{timeConverter(course.start_time)} to {timeConverter(course.end_time)}</h3>
-              <h3>Starts <strong>{getDateStringENGBFormat(course.first_day)}</strong>
-              <br />Ends <strong>{getDateStringENGBFormat(course.last_day)}</strong></h3>
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="course-card">
+        <p>{studentCourse.length} course(s)</p>
+        {!studentCourse || studentCourse.length === 0 ? (
+          <p>{student.first_name} has not registered for any courses yet.</p>
+        ) : (
+          <div className="coursesList">
+            {studentCourse.map(course => (
+              <div key={course.course_id}>
+                <p>{course.group_type}</p>
+                <p>
+                  {course.course_days}
+                  {timeConverter(course.start_time)} to {timeConverter(course.end_time)}
+                </p>
+                <p>
+                  Starts{getDateStringENGBFormat(course.first_day)}
+                  Ends{getDateStringENGBFormat(course.last_day)}
+                </p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-  )
-};
+  );
+}
 
 export default StudentCourseCard;
