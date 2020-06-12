e103783b76c8130934b224ede697fe8b0e19348d
Alec Dye <alecdye@gmail.com>
2020-06-12 09:58:20 -0700

    Fix username display on dashboard

--- src/views/userDashboard/components/UserDashboard.js
+++ src/views/userDashboard/components/UserDashboard.js
@@ -1,44 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import { withRouter, Link, useHistory, useRouteMatch } from 'react-router-dom';
+import { withRouter, Link, useRouteMatch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import axiosWithAuth from '../../../utils/axiosWithAuth';
 
 import StudentCourseCard from './Student/StudentCourseCard';
 
-import '../userDashboard.scss'
-
+import '../userDashboard.scss';
 
 import { getStudentsInFamily } from '../getStudentsinFamily';
 import { getMessagesForUser } from '../getMessagesForUser';
 
-
-
 function UserDashboard(props) {
   const [userData, setUserData] = useState({
-    userID: null,
+    subject: null,
     name: '',
     students: [],
     messages: [],
   });
+  const [user, setUser] = useState({});
 
   let { url } = useRouteMatch();
 
-  // Get userID from JWT
+  // Get subject from JWT
   let token = localStorage.getItem('token');
   let tokenData = JSON.parse(atob(token.split('.')[1]));
 
-  let userID = tokenData.subject;
-  let name = tokenData.name;
+  const { subject } = tokenData;
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/users/${subject}`)
+      .then(res => {
+        setUser(res.data[0]);
+      })
+      .catch(err => {
+        console.log('whoops', err);
+      });
+  }, []);
 
   // retrieve list of students associated with this account (search by user ID)
   useEffect(() => {
-    getStudentsInFamily(userID)
+    getStudentsInFamily(subject)
       .then(result => {
         setUserData({ students: result });
       })
       .catch(error => {
         console.log('Error in retrieving students:', error);
       });
-  }, [props.studentTableReducer.needToUpdateStudentList]);
+  }, [subject, props.studentTableReducer.needToUpdateStudentList]);
 
   // update userData once students have been updated
   useEffect(() => {
@@ -52,23 +61,23 @@ function UserDashboard(props) {
   // if userData hasn't loaded yet, return a loading message/icon
   if (Object.keys(userData).length === 0 || !userData.students || !userData.messages) {
     return <h2>Loading...</h2>;
-  } else if (!userID) {
+  } else if (!subject) {
     return <h2>Invalid user ID</h2>;
   }
-  
+
   return (
     <div className="userDashboard ">
-      <h1>Welcome, {name}.</h1>
+      <h1>Welcome, {user.name}.</h1>
 
       {userData.students.map((student, id) => (
         <Link to={`/student/${student.id}`}>
-          <StudentCourseCard student={student} />
+          <StudentCourseCard key={student.id} student={student} />
         </Link>
       ))}
 
       <Link to={`${url}/student-register`}>
         <div className="iconContainer">
-              <i className="fas fa-user-plus add-icon"></i>
+          <i className="fas fa-user-plus add-icon"></i>
         </div>
       </Link>
     </div>
