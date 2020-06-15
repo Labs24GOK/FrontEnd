296a2d0db28858915e4b752e9682dfc47e3a6fc9
Alec Dye <alecdye@gmail.com>
2020-06-15 13:45:30 -0700

    Add testObject items 1-25

---
+++ src/views/userDashboard/components/placementTest/Child/TestObject.js
@@ -0,0 +1,433 @@
+const TestObject = () => [
+  {
+    key: 1,
+    id: 1,
+    question: 'Five',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 2,
+    id: 2,
+    question: 'Pencil',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 3,
+    id: 3,
+    question: 'Ball',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 4,
+    id: 4,
+    question: 'A big dog',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 5,
+    id: 5,
+    question: 'Eyes',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 6,
+    id: 6,
+    question: 'I have a long tail',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 7,
+    id: 7,
+    question: 'Socks',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 8,
+    id: 8,
+    question: 'I cannot ride a bike',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 9,
+    id: 9,
+    question: 'Bedroom',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 10,
+    id: 10,
+    question: 'I like ice cream',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 11,
+    id: 11,
+    question: 'Eighteen',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 12,
+    id: 12,
+    question: 'These are kites',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 13,
+    id: 13,
+    question: 'Lamp',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 14,
+    id: 14,
+    question: 'They are eating cake',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 15,
+    id: 15,
+    question: 'Juice',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 16,
+    id: 16,
+    question: 'It can swim and jump',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 17,
+    id: 17,
+    question: 'Basketball',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 18,
+    id: 18,
+    question: 'The carrots are between the tomatoes and plums',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 19,
+    id: 19,
+    question: 'Grandparents',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 20,
+    id: 20,
+    question: 'She is getting dressed',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 21,
+    id: 21,
+    question: 'Fifty',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 22,
+    id: 22,
+    question: 'He works on a farm',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 23,
+    id: 23,
+    question: 'Backache',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 24,
+    id: 24,
+    question: 'He is very strong',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+    key: 25,
+    id: 25,
+    question: 'Snow',
+    choices: ['a', 'b', 'c'],
+    images: [
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+      '../../../../../assets/Logo.png',
+    ],
+  },
+];
+
+export default TestObject;
+
+// 1. Five
+// B.
+// C.
+// A.
+// 2. Pencil
+// B.
+// A.
+// C.
+// 3. Ball
+// A.
+// B.
+// C.
+// 4. A big dog
+// A.
+// B.
+// C.
+// 5. Eyes
+// A
+// B
+// C
+// 6. I've got a long tail
+// A.
+// B.
+// C.
+// 7. Socks
+// A.
+// B.
+// C.
+// 8. I can't ride a bike
+// A
+// B
+// C
+// 9. Bedroom
+// A
+// B
+// C
+// 10. I like ice cream
+// A
+// B
+// C
+// 11. eighteen
+// A
+// B
+// C
+// 12. These are kites
+// A
+// B
+// C
+// 13. Lamp
+// A
+// B
+// C
+// 14. They're eating cake
+// A
+// B
+// C
+// 15. Juice
+// A
+// B
+// C
+// 16. It can swim and jump
+// A
+// B
+// C
+// 17. Basketball
+// A
+// B
+// C
+// 18. The carrots are between the tomatoes and plums
+// A.
+// B
+// C
+// 19. Grandparents
+// A
+// B
+// C
+// 20. She's getting dressed
+// A
+// B
+// C
+// 21. fifty
+// A
+// B
+// C
+// 22. He works on a farm
+// A
+// B
+// C
+// 23. Backache
+// A
+// B
+// C
+// 24. He's very strong
+// A
+// B
+// C
+// 25. Snow
+// A
+// B
+// C
+
+// 26.
+// She's exciting.
+// She's difficult.
+// She's busy.
+// 27.
+// He's running quickly.
+// He's running good.
+// He's running quick.
+// 28.
+// He go to the doctor yesterday.
+// He goed to the doctor yesterday.
+// He went to the doctor yesterday.
+// 29. 12th
+// Twelve.
+// Twelfth.
+// Twelveth.
+// 30.
+// I could read when I was six.
+// I couldn't read when I am six.
+// I can read when I was six.
+// 31.
+// The horse is thirsty than the dog.
+// The horse is thirster than the dog.
+// The horse is thirstier than the dog.
+// 32.
+// Giraffes are the taller animals in the world.
+// Giraffes are the tallest animals in the world.
+// Giraffes are the tallest than animals in the world.
+// 33.
+// He didn't ate sandwiches for lunch.
+// He not eat sandwiches for lunch.
+// He didn't eat sandwiches for lunch.
+// 34.
+// The man walked the library.
+// The man walked into the library.
+// The man walked on the library.
+// 35.
+// It's half past two.
+// It's quarter past two.
+// It's quarter to two.
+// 36.
+// They're going to catch the bus.
+// They going to catch the bus.
+// They go to catch the bus.
+// 37.
+// This is the room which you brush your teeth.
+// This is the room where you brush your teeth.
+// This is the room who you brush your teeth.
+// 38.
+// He was eat dinner at 8 o' clock.
+// He was ate dinner at 8 o' clock.
+// He was eating dinner at 8 o' clock.
+// 39.
+// The first month of the year is January.
+// The first month of the year is July.
+// The first mont of the year is June.
+// 40.
+// Is paper made to wood?
+// Is paper made of wood?
+// Is paper made in wood?
+// 41.
+// The tea looks like hot.
+// The tea look hot.
+// The tea looks hot.
+// 42.
+// You shouldn't to carry heavy bags.
+// You shouldn't carried heavy bags.
+// You shouldn't carry heavy bags.
+// 43.
+// She win the match.
+// She's won the match.
+// She's winned the match.
+// 44.
+// Will the rocket go to the moon.
+// Will the rocket goes to the moon.
+// Will the rocket going to the moon.
+// 45.
+// They were walked in the forest when they seeing an owl.
+// They’re walking in the forest when they’re seeing an owl.
+// They were walking in the forest when they saw an owl.
+// 46.
+// There are enough foods to eat.
+// There is enough food to eat.
+// There are enough food to eat.
+// 47.
+// The door bell rang but no-one was at the door.
+// The door bell rang but anyone was at the door.
+// The door bell rang but everyone was at the door.
+// 48.
+// They might need their coats.
+// They might to need their coats.
+// They may to need their coats.
+// 49.
+// He's excited.
+// He's afraid.
+// He's brave.
+// 50.
+// I’m never eating Italian food before.
+// I never eat Italian food before.
+// I’ve never eaten Italian food before.
