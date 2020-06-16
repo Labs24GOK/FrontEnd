d2f086b18292ffab77ee6c72a815a3656c94dd2e
Alec Dye <alecdye@gmail.com>
2020-06-16 13:06:40 -0700

    Add style to radio group

--- src/views/userDashboard/components/placementTest/Child/ChildQuestions.js
+++ src/views/userDashboard/components/placementTest/Child/ChildQuestions.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Button, Radio } from 'antd';
 
-const ChildQuestions = (props) => {
+const ChildQuestions = props => {
   const [form] = Form.useForm();
-  const nextQuestion = props.nextQuestion
-  const { question, images, choices } = props.currentQuestion[0] // currently a array in store
+  const nextQuestion = props.nextQuestion;
+  const { question, images, choices } = props.currentQuestion[0]; // currently a array in store
+
+  const radioStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '2rem 0',
+  };
 
   return (
     <Form form={form}>
@@ -14,7 +22,7 @@ const ChildQuestions = (props) => {
         <img src={images[1]} alt="image" />
         <img src={images[2]} alt="image" />
       </div>
-      <Radio.Group>
+      <Radio.Group style={radioStyle}>
         <Radio value={'a'}>{choices[0]}</Radio>
         <Radio value={'b'}>{choices[1]}</Radio>
         <Radio value={'c'}>{choices[2]}</Radio>
