a905bea0a972956a02d5c849d448163844615770
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-06-17 13:56:01 -0700

    added grading function

--- src/views/userDashboard/components/placementTest/Child/ChildPlacementTest.js
+++ src/views/userDashboard/components/placementTest/Child/ChildPlacementTest.js
@@ -5,6 +5,7 @@ import StartTest from './StartTest';
 import ChildQuestions from './ChildQuestions'
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { nextPage, prevPage, getChildQuestions, startTestTimer, timeOut } from '../../../../../actions/userDashboardActions/placementActions'
+import { rubric as grade } from './rubric'
 
 const ChildPlacementTest = props => {
   const dispatch = useDispatch()
@@ -39,11 +40,24 @@ const ChildPlacementTest = props => {
     dispatch(getChildQuestions())
   }, [])
 
+  const phaseOneGradeHelper = () => {
+    let score = 0;
+    userAwnsers.map((awnser, index) => {
+      console.log("score map", awnser, index)
+      if(awnser == grade[index]) {
+        return score = score + 1;
+      }
+    })
+    return score
+  }
+
   const testHelper = () => {
     if(page === 0) {
       return <StartTest />
     }  else if (page >= 1) {
       return <ChildQuestions currentQuestion={currentQuestion} />
+    } else if (page >= 25) {
+      
     }
   }
 
@@ -51,6 +65,7 @@ const ChildPlacementTest = props => {
     <div>
       { questions ? testHelper() : (<h1>LOADING...</h1>) }
       <Button onClick={() => console.log("STATE LOGS ", page, currentQuestion, userAwnsers)}>LOG</Button>
+      <Button onClick={() => console.log("score", phaseOneGradeHelper())}>Score</Button>
     </div>
   );
 };
