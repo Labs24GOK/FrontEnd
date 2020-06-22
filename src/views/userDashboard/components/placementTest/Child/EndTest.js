120667ff3e40228f84b8086e0a763f1e1b4dab53
Alec Dye <alecdye@gmail.com>
2020-06-22 10:45:12 -0700

    Scaffolding end test

---
+++ src/views/userDashboard/components/placementTest/Child/EndTest.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Rendering conditions:
+// 1. Test timer expired -> pushed here with explaination and warning about remaining amount of retries left
+// time valid = true/false? if timeValid = false render
+// 2. 3/3 retries attempted -> polite message to contact admin about scheduling an in-person test
+// attempt = 1, 2, 3, 4; if attempt > 3 render
+// 3. Test completed successfully -> Recommended course with scheduling links
+
+function EndTest() {
+  return <div></div>;
+}
+
+export default EndTest;
