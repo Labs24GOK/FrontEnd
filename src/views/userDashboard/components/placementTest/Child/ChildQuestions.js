7bf179e0e3bd3f86fbcc09e11a715bbfc01632e7
Alec Dye <alecdye@gmail.com>
2020-06-22 10:25:19 -0700

    Build test countdown timer

--- src/views/userDashboard/components/placementTest/Child/ChildQuestions.js
+++ src/views/userDashboard/components/placementTest/Child/ChildQuestions.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+import TestTimer from './TestTimer';
 import { nextPage } from '../../../../../actions/userDashboardActions/placementActions';
 import { PrimaryButton } from '../../../../../styles/BtnStyle';
 import {
@@ -15,12 +16,18 @@ import {
 const ChildQuestions = props => {
   const dispatch = useDispatch();
   const { register, handleSubmit, errors, reset } = useForm();
-  const { question, images, choices, option } = props.currentQuestion[0]; // currently a array in store
+  const { question, images, choices, option, page } = props.currentQuestion[0]; // currently a array in store
 
   const onSubmit = data => {
     dispatch(nextPage(data.userChoice));
     reset();
   };
+
+  // const testTimerStart = page => {
+  //   if (page === 1) {
+  //     return <TestTimer />
+  //   }
+  // }
 
   const imageChecker = images => {
     if (images.length === 0) {
@@ -86,6 +93,7 @@ const ChildQuestions = props => {
 
   return (
     <div className="test-wrapper">
+      <TestTimer />
       <Instruction>Choose the answer that best matches the phrase:</Instruction>
       <Question>{question}</Question>
       <form onSubmit={handleSubmit(onSubmit)}>
